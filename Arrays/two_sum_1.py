c987e7504b3518be358e34ead2699f889db6e263
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-21 16:57:26 -0400

    Solve 1 - Two Sum

--- Arrays/two_sum_1.py
+++ Arrays/two_sum_1.py
@@ -11,11 +11,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        return []
+        dictionary = {}
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in dictionary:
+                return [dictionary[complement], idx]
+
+            dictionary[num] = idx
 
 
 def main():
-    print(Solution().twoSum([], 0))
+    print(Solution().twoSum([3, 2, 4], 6))
 
 
 if __name__ == "__main__":
