54df455e771195473c204cbec096aa87585b520b
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-21 16:07:59 -0400

    1 - Two Sum: Initial commit

---
+++ Arrays/two_sum_1.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/two-sum/
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().twoSum([], 0))
+
+
+if __name__ == "__main__":
+    main()
