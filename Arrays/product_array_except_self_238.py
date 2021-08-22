3c67d3cea8ebebc30830cce82599a149143665b8
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-22 17:31:10 -0400

    238 - Product of Array Except Self: Initial commit

---
+++ Arrays/product_array_except_self_238.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/product-of-array-except-self/
+"""
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().productExceptSelf([1, 2, 3, 4]))
+
+
+if __name__ == "__main__":
+    main()
