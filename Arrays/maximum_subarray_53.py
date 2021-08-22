6395d1b1469959fbbaf169c32be8f99e1cfefcb1
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-22 11:46:50 -0400

    53: Maximum Subarray - Initial commit

---
+++ Arrays/maximum_subarray_53.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-subarray/
+"""
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 0
+
+
+def main():
+    print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+
+if __name__ == "__main__":
+    main()
