81cfca31fe59d4245c9fe8a3fc3bf03d07f0c31f
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-22 16:14:15 -0400

    Solve 53: Maximum Subarray

--- Arrays/maximum_subarray_53.py
+++ Arrays/maximum_subarray_53.py
@@ -10,11 +10,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return 0
+        max_sum = nums[0]
+        sums_so_far = [nums[0]]
+        for idx, num in enumerate(nums[1:]):
+            sums_so_far.append(max(num, num + sums_so_far[idx]))
+            max_sum = max(max_sum, sums_so_far[-1])
+
+        return max_sum
 
 
 def main():
-    print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(Solution().maxSubArray([-2, -1, -3, -4, -1, -2, -1, -5, -4]))
 
 
 if __name__ == "__main__":
