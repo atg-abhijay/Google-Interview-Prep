8f8f277ffe198b7961a3254c1f421a0a54fc9dc6
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-22 16:19:22 -0400

    152: Maximum Product Subarray - Initial commit

---
+++ Arrays/maximum_product_subarray_152.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-product-subarray/
+"""
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 0
+
+
+def main():
+    print(Solution().maxProduct([-2, 0, -1]))
+
+
+if __name__ == "__main__":
+    main()
