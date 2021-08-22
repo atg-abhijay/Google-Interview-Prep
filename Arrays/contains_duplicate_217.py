787c8776f980fc9c98ddf6dd659517e8bb7a7d42
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-21 23:23:19 -0400

    217 - Contains Duplicate: Initial commit

---
+++ Arrays/contains_duplicate_217.py
@@ -0,0 +1,19 @@
+"""
+URL of problem:
+https://leetcode.com/problems/contains-duplicate/
+"""
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().containsDuplicate([]))
+
+
+main()
