a1717de4cb4e26be2df65bfb7c0c19b1a14d5331
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-22 11:15:33 -0400

    121 - Best Time to Buy and Sell Stock: Initial commit

---
+++ Arrays/best_time_buy_sell_stock_121.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        return 0
+
+
+def main():
+    print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
+
+
+if __name__ == "__main__":
+    main()
