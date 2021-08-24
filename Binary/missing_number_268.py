23c614bed77ddf0e968958dda3c3cfdc7a34e01d
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 00:34:58 -0400

    268 - Missing Number: Initial commit

---
+++ Binary/missing_number_268.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/missing-number/
+"""
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
+
+if __name__ == "__main__":
+    main()
