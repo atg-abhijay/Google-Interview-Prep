bb414a4c7d173db4c98c3251a1b7e436c41cd81c
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 12:56:26 -0400

    70 - Climbing Stairs: Initial commit

---
+++ Dynamic Programming/climbing_stairs_70.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/climbing-stairs/
+"""
+
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().climbStairs(3))
+
+
+if __name__ == "__main__":
+    main()
