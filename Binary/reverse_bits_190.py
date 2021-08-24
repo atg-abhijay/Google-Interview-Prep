dae85064cee58c1c01780ab6d73af42bb8a8bce9
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 01:21:40 -0400

    190 - Reverse Bits: Initial commit

---
+++ Binary/reverse_bits_190.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/reverse-bits/
+"""
+
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return -1
+
+
+def main():
+    print(Solution().reverseBits(4294967293))
+
+
+if __name__ == "__main__":
+    main()
