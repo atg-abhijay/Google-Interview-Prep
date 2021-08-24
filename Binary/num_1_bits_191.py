444e03943fd8b8dc7546d436046987ee8d4e1702
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-23 23:53:34 -0400

    191 - Number of 1 Bits - Initial commit

---
+++ Binary/num_1_bits_191.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/number-of-1-bits/
+"""
+
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return 0
+
+
+def main():
+    print(Solution().hammingWeight(11111111111111111111111111111101))
+
+
+if __name__ == "__main__":
+    main()
