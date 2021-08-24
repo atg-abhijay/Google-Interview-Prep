2be7d9ac6c6e32848a4fc22f4dbe77479f66eaf3
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 19:37:46 -0400

    Solve 198 - House Robber

--- Dynamic Programming/house_robber_198.py
+++ Dynamic Programming/house_robber_198.py
@@ -10,11 +10,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return -1
+        nums = [0, 0] + nums
+        num_elems = len(nums)
+        max_profits = [0] * (num_elems)
+        for idx in range(2, num_elems):
+            max_profits[idx] = max(
+                max_profits[idx - 1], nums[idx] + max_profits[idx - 2]
+            )
+
+        return max_profits[-1]
 
 
 def main():
-    print(Solution().rob([2,7,9,3,1]))
+    print(Solution().rob([2, 7, 9, 3, 1]))
 
 
 if __name__ == "__main__":
