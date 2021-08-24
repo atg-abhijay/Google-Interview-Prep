72ab42fd21ccd391f7775987f30fc8d02c134886
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 16:51:00 -0400

    322 - Coin Change: Initial commit

---
+++ Dynamic Programming/coin_change_322.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/coin-change/
+"""
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().coinChange([1, 2, 5], 11))
+
+
+if __name__ == "__main__":
+    main()
