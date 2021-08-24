29f0d29aa92d042234b724971e53af82703bdcc9
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 19:37:03 -0400

    198 - House Robber: Initial commit

---
+++ Dynamic Programming/house_robber_198.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/house-robber/
+"""
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().rob([2,7,9,3,1]))
+
+
+if __name__ == "__main__":
+    main()
