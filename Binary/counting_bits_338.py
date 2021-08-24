5538ea99f474ada1124c15d02c50375059409909
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 00:15:11 -0400

    338 - Counting Bits: Initial commit

---
+++ Binary/counting_bits_338.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/counting-bits/
+"""
+
+
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().countBits(5))
+
+
+if __name__ == "__main__":
+    main()
