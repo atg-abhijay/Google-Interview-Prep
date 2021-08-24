11e70f58923fa4fd31c67164e9ae397b3a96e00a
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 01:08:58 -0400

    Solve 268 - Missing Number using binary operations
    Add a solution using functools.reduce as well

---
+++ Binary/missing_number_268_binary.py
@@ -0,0 +1,32 @@
+"""
+URL of problem:
+https://leetcode.com/problems/missing-number/
+"""
+
+
+from functools import reduce
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Alternate solution:
+        # return reduce(
+        #     lambda xor, tup: xor ^ tup[0] ^ tup[1], enumerate(nums), len(nums)
+        # )
+        xor_value = len(nums)
+        for idx, num in enumerate(nums):
+            xor_value ^= idx ^ num
+
+        return xor_value
+
+
+def main():
+    print(Solution().missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
+
+if __name__ == "__main__":
+    main()
