a72b2b22f016f7dd060739f34ba0da15cf280260
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-25 18:00:57 -0400

    39 - Combination Sum: Initial commit

---
+++ Dynamic Programming/combination_sum_39.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/combination-sum/
+"""
+
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().combinationSum([2, 3, 5], 8))
+
+
+if __name__ == "__main__":
+    main()
