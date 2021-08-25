d4e98b5bc5a30e2b741c346a280ebaa9360512c4
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-25 17:00:48 -0400

    55 - Jump Game: Initial commit

---
+++ Dynamic Programming/jump_game_55.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/jump-game/
+"""
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().canJump([3, 2, 1, 0, 4]))
+
+
+if __name__ == "__main__":
+    main()
