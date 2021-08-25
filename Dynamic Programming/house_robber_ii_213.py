6e5d4079462211e5e5d7bc9146a9f6a275210eeb
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-24 20:09:45 -0400

    213 - House Robber II: Initial commit

---
+++ Dynamic Programming/house_robber_ii_213.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+
+"""
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().rob([2, 3, 2]))
+
+
+if __name__ == "__main__":
+    main()
