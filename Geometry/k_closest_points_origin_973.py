525773fba2ec839bbfc246d62c0d192fb38e46f4
Abhijay Gupta <atg.abhijay@gmail.com>
2021-08-26 10:37:55 -0400

    973 - K Closest Points to Origin: Initial commit

---
+++ Geometry/k_closest_points_origin_973.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/k-closest-points-to-origin/
+"""
+
+
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().kClosest([[3,3],[5,-1],[-2,4]], 2))
+
+
+if __name__ == "__main__":
+    main()
