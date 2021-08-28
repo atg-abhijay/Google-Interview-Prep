fef85457c5ce6fdb75722b180526d33544a34e29
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-28 16:11:57 -0400

    133. Clone Graph: Initial commit

---
+++ Graph/clone_graph_133.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/clone-graph/
+"""
+
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        return None
