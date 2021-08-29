e5d5e82178668cf5586198d529a497cda8539ba8
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-29 14:28:15 -0400

    417. Pacific Atlantic Water Flow: Initial commit

---
+++ Graph/pacific_atlantic_water_flow_417.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/pacific-atlantic-water-flow/
+"""
+
+
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().pacificAtlantic([[2, 1], [1, 2]]))
+
+
+if __name__ == "__main__":
+    main()
