2385f0bebd434d01f7a5d179d48f1a86b44ad710
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-30 12:26:01 -0400

    1128. Number of Equivalent Domino Pairs: Initial commit

---
+++ Hash Table/num_eq_domino_pairs_1128.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+"""
+
+
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        """
+        :type dominoes: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().numEquivDominoPairs([[1, 2], [2, 1], [3, 4], [5, 6]]))
+
+
+if __name__ == "__main__":
+    main()
