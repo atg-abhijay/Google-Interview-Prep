fd08ca09fec983cca2432cbd10b9afe38da509e9
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-30 16:12:56 -0400

    442. Find All Duplicates in an Array: Initial commit

---
+++ Hash Table/duplicates_in_array_442.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/find-all-duplicates-in-an-array/
+"""
+
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]))
+
+
+if __name__ == "__main__":
+    main()
