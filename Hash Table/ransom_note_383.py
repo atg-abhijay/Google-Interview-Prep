85951f39acecc8a7a93f007201852b63ca714dec
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-30 12:23:30 -0400

    383. Ransom Note: Initial commit

---
+++ Hash Table/ransom_note_383.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/ransom-note/
+"""
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().canConstruct("aa", "aab"))
+
+
+if __name__ == "__main__":
+    main()
