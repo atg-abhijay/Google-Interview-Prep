a498f9feee25e54d5cddc7e3712675925ed07a93
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-30 16:46:30 -0400

    1418. Display Table of Food Orders in a Restaurant: Initial commit

---
+++ Hash Table/display_orders_in_restaurant_1418.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
+"""
+
+
+class Solution(object):
+    def displayTable(self, orders):
+        """
+        :type orders: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().displayTable([["David","3","Ceviche"],["Corina","10","Beef Burrito"],["David","3","Fried Chicken"],["Carla","5","Water"],["Carla","5","Ceviche"],["Rous","3","Ceviche"]]))
+
+
+if __name__ == "__main__":
+    main()
