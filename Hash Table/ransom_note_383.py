98b0ee726e7d89e6f4ec64d469007f77a5e4386a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-30 12:24:11 -0400

    Solve 383. Ransom Note

--- Hash Table/ransom_note_383.py
+++ Hash Table/ransom_note_383.py
@@ -11,8 +11,22 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        return False
+        rsm_letters, mzn_letters = dict(), dict()
+        for string, dictionary in [[ransomNote, rsm_letters], [magazine, mzn_letters]]:
+            for letter in string:
+                if letter not in dictionary:
+                    dictionary[letter] = 1
+                else:
+                    dictionary[letter] += 1
 
+        for letter, count in rsm_letters.items():
+            if letter not in mzn_letters:
+                return False
+
+            if count > mzn_letters[letter]:
+                return False
+
+        return True
 
 def main():
     print(Solution().canConstruct("aa", "aab"))
