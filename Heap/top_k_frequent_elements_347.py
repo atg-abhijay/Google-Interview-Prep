424b5332bd85789c0eb2d9bb369580e212fc8164
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-08-31 22:27:22 -0400

    347. Top K Frequent Elements: Initial commit

---
+++ Heap/top_k_frequent_elements_347.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/top-k-frequent-elements/
+"""
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().topKFrequent([1, 1, 1, 2, 2, 3], 2))
+
+
+if __name__ == "__main__":
+    main()
