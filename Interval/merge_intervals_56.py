189074591658c3fdb43651750de7e57e683f9b01
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-01 17:28:32 -0400

    56. Merge Intervals: Initial commit

---
+++ Interval/merge_intervals_56.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/merge-intervals/
+"""
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+
+
+if __name__ == "__main__":
+    main()
