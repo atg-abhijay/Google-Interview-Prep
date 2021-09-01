3121c92249110428cdbdd44173ce9b21b15e394a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-01 16:48:08 -0400

    Solve 57. Insert Interval

--- Interval/insert_interval_57.py
+++ Interval/insert_interval_57.py
@@ -11,11 +11,52 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        return [[]]
+        if not intervals:
+            return [newInterval]
+
+        did_overlap_occur = False
+        overlap_start, overlap_end = -1, len(intervals) - 1
+        for idx, intvl in enumerate(intervals):
+            if self.is_overlap(intvl, newInterval):
+                if overlap_start == -1:
+                    overlap_start = idx
+
+                newInterval = self.merge_intervals(intvl, newInterval)
+                did_overlap_occur = True
+
+            # Overlaps are finished
+            elif did_overlap_occur:
+                overlap_end = idx-1
+                break
+
+            # No overlaps encountered yet
+            else:
+                if newInterval[0] < intvl[0]:
+                    intervals.insert(idx, newInterval)
+                    return intervals
+
+        if did_overlap_occur:
+            for idx in range(overlap_end, overlap_start, -1):
+                intervals.pop(idx)
+
+            intervals[overlap_start] = newInterval
+            return intervals
+
+        # Case where interval is at the end of the list
+        intervals.append(newInterval)
+        return intervals
+
+
+    def is_overlap(self, intvl_a, intvl_b):
+        return intvl_a[0] <= intvl_b[1] and intvl_b[0] <= intvl_a[1]
+
+
+    def merge_intervals(self, intvl_a, intvl_b):
+        return [min(intvl_a[0], intvl_b[0]), max(intvl_a[1], intvl_b[1])]
 
 
 def main():
-    print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
+    print(Solution().insert([[1, 5]], [2, 3]))
 
 
 if __name__ == "__main__":
