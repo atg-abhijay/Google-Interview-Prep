db33b231ebed554ddc01dde32b32715dc4a085a9
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-01 18:13:53 -0400

    Solve 56. Merge Intervals

--- Interval/merge_intervals_56.py
+++ Interval/merge_intervals_56.py
@@ -10,11 +10,26 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        return [[]]
+        intervals.sort(key=lambda x: x[0])
+        merged_intervals = []
+        prev_interval = [-1, -1]
+        for intvl in intervals:
+            if self.is_overlap(intvl, prev_interval):
+                merged_intervals[-1] = [min(intvl[0], prev_interval[0]), max(intvl[1], prev_interval[1])]
+            else:
+                merged_intervals.append(intvl)
+
+            prev_interval = merged_intervals[-1]
+
+        return merged_intervals
+
+
+    def is_overlap(self, intvl_a, intvl_b):
+        return intvl_a[0] <= intvl_b[1] and intvl_b[0] <= intvl_a[1]
 
 
 def main():
-    print(Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+    print(Solution().merge([[4,5],[2,4],[4,6],[3,4],[0,0],[1,1],[3,5],[2,2]]))
 
 
 if __name__ == "__main__":
