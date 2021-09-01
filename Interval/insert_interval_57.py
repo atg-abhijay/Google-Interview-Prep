c2f680a45782e00779ef54b950d15138597927a1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-01 15:52:20 -0400

    57. Insert Interval: Initial commit

---
+++ Interval/insert_interval_57.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/insert-interval/
+"""
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
+
+
+if __name__ == "__main__":
+    main()
