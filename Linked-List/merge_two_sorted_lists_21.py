29f7a3724953abc22119e258882f27329d5f466c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 16:45:53 -0400

    Solve 21. Merge Two Sorted Lists

--- Linked-List/merge_two_sorted_lists_21.py
+++ Linked-List/merge_two_sorted_lists_21.py
@@ -18,4 +18,32 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        return None
+        if not l1 and not l2:
+            return None
+
+        head, current_node = None, None
+        did_one_list_end = False
+        while 1:
+            if not all([l1, l2]):
+                new_node = l1 or l2
+                did_one_list_end = True
+            elif l1.val < l2.val:
+                new_node = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                new_node = ListNode(l2.val)
+                l2 = l2.next
+
+            if not head:
+                head = new_node
+
+            if not current_node:
+                current_node = new_node
+            else:
+                current_node.next = new_node
+                current_node = new_node
+
+            if did_one_list_end:
+                break
+
+        return head
