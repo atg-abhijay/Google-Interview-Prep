4275acdf453bb980c1e37bf7a85cd06bb695b492
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 16:31:54 -0400

    21. Merge Two Sorted Lists: Initial commit

---
+++ Linked-List/merge_two_sorted_lists_21.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/merge-two-sorted-lists/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        return None
