7b8e427b6fb2fd978ffe052242ffbc62c3c7f0b2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 11:22:15 -0400

    206. Reverse Linked List: Initial commit

---
+++ Linked-List/reverse_linked_list_206.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/reverse-linked-list/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        return None
