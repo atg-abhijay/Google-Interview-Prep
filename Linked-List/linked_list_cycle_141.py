070aeeb4381321d486b85dc8addeda08c1717315
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 16:30:07 -0400

    Solve 141. Linked List Cycle

--- Linked-List/linked_list_cycle_141.py
+++ Linked-List/linked_list_cycle_141.py
@@ -17,4 +17,18 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        if not head:
+            return False
+
+        slow_ptr, fast_ptr = head, head
+        while 1:
+            if fast_ptr.next and fast_ptr.next.next:
+                fast_ptr = fast_ptr.next.next
+            else:
+                return False
+
+            slow_ptr = slow_ptr.next
+            if slow_ptr == fast_ptr:
+                return True
+
         return False
