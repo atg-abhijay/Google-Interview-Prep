a7503e6feb44af98921670a49773dc027e72c19c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 16:53:20 -0400

    23. Merge k Sorted Lists: Initial commit

---
+++ Linked-List/merge_k_sorted_lists_23.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/merge-k-sorted-lists/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        return None
