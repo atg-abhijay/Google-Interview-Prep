8e0679576b3ef10d7a2600aeba76210549f6e500
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 14:25:50 -0400

    141. Linked List Cycle: Initial commit

---
+++ Linked-List/linked_list_cycle_141.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/linked-list-cycle/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        return False
