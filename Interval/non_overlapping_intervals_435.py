70adf113bb79a16ff04fb44d2ca0f1a0722d6e3b
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-01 23:08:06 -0400

    435. Non-overlapping Intervals: Initial commit

---
+++ Interval/non_overlapping_intervals_435.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/non-overlapping-intervals/
+"""
+
+
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
+
+
+if __name__ == "__main__":
+    main()
