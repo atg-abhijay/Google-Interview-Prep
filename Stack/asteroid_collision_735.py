163562e834b06a3961af07a2b8d1733a865303d2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 22:57:55 -0400

    735. Asteroid Collision: Initial commit

---
+++ Stack/asteroid_collision_735.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/asteroid-collision/
+"""
+
+
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().asteroidCollision([10, 2, -5]))
+
+
+if __name__ == "__main__":
+    main()
