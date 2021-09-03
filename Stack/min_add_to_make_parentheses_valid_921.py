e161b78e311b564efb51e51ef95e89e4f8963fff
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 22:05:45 -0400

    921. Minimum Add to Make Parentheses Valid: Initial commit

---
+++ Stack/min_add_to_make_parentheses_valid_921.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+"""
+
+
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().minAddToMakeValid("()))(("))
+
+
+if __name__ == "__main__":
+    main()
