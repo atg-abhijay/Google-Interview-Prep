4f6de4a884a1d1863e0b2c92c34c090d71f9d3b2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 22:36:08 -0400

    Solve 1472. Design Browser History

--- Stack/design_browser_history_1472.py
+++ Stack/design_browser_history_1472.py
@@ -9,24 +9,41 @@ class BrowserHistory(object):
         """
         :type homepage: str
         """
+        self.history = [homepage]
+        self.current_idx = 0
 
     def visit(self, url):
         """
         :type url: str
         :rtype: None
         """
+        self.history[self.current_idx + 1 :] = []
+        self.history.append(url)
+        self.current_idx += 1
 
     def back(self, steps):
         """
         :type steps: int
         :rtype: str
         """
+        if steps > self.current_idx:
+            self.current_idx = 0
+            return self.history[0]
+
+        self.current_idx -= steps
+        return self.history[self.current_idx]
 
     def forward(self, steps):
         """
         :type steps: int
         :rtype: str
         """
+        if steps > len(self.history) - (self.current_idx + 1):
+            self.current_idx = len(self.history) - 1
+            return self.history[-1]
+
+        self.current_idx += steps
+        return self.history[self.current_idx]
 
 
 # Your BrowserHistory object will be instantiated and called as such:
