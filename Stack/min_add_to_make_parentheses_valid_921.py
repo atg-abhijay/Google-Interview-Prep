3633a1a26cc0b26b49ccc2c2e92b37b504232011
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 22:14:12 -0400

    Solve 921. Minimum Add to Make Parentheses Valid

--- Stack/min_add_to_make_parentheses_valid_921.py
+++ Stack/min_add_to_make_parentheses_valid_921.py
@@ -10,7 +10,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        return -1
+        unmatched_closing_bkts = 0
+        stack_size = 0
+        for char in s:
+            if char == '(':
+                stack_size += 1
+            else:
+                if not stack_size:
+                    unmatched_closing_bkts += 1
+                else:
+                    stack_size -= 1
+
+        return stack_size + unmatched_closing_bkts
 
 
 def main():
