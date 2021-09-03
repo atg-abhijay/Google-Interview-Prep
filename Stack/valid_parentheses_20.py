2266a931749e7e4a58ab193a66aea2fb42f9ec36
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 21:46:47 -0400

    20. Valid Parentheses: Initial commit

---
+++ Stack/valid_parentheses_20.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/valid-parentheses/
+"""
+
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().isValid("()[]{}"))
+
+
+if __name__ == "__main__":
+    main()
