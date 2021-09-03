47603bb8263dd8e7baa76913a1e548984f92e89f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-02 23:49:09 -0400

    Solve 735. Asteroid Collision

--- Stack/asteroid_collision_735.py
+++ Stack/asteroid_collision_735.py
@@ -10,7 +10,35 @@ class Solution(object):
         :type asteroids: List[int]
         :rtype: List[int]
         """
-        return []
+        stack = []
+        for asterd in asteroids:
+            # Last asteroid was going right
+            # and current asteroid is going left
+            if all([stack and stack[-1] > 0, asterd < 0]):
+                is_resolved = False
+                while not is_resolved:
+                    is_resolved = self.resolveCollision(stack, asterd)
+            else:
+                stack.append(asterd)
+
+        return stack
+
+
+    def resolveCollision(self, stack, asterd):
+        # asterd (going left) will not
+        # collide with an asteroid going left
+        if not stack or stack[-1] < 0:
+            stack.append(asterd)
+        else:
+            last_asterd = stack[-1]
+            if last_asterd == abs(asterd):
+                stack.pop()
+
+            elif last_asterd < abs(asterd):
+                stack.pop()
+                return False
+
+        return True
 
 
 def main():
