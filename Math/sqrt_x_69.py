36a168356d965a0b73775c5462e1fd011d519006
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-04 15:45:55 -0400

    69. Sqrt(x): Initial commit

---
+++ Math/sqrt_x_69.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/sqrtx/
+"""
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return 0
+
+
+def main():
+    print(Solution().mySqrt(4))
+
+
+if __name__ == "__main__":
+    main()
