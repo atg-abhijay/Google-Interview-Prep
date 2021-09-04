828ca433efa7dc044458fd5063aef951a2c68f8d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-04 16:00:28 -0400

    Solve 69. Sqrt(x)

--- Math/sqrt_x_69.py
+++ Math/sqrt_x_69.py
@@ -6,11 +6,22 @@ https://leetcode.com/problems/sqrtx/
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return 0
+        lower, upper = 0, x
+        while lower <= upper:
+            middle = (lower + upper) / 2
+            square = int(middle * middle)
+            if square > x:
+                upper = middle
+            elif square < x:
+                lower = middle
+            else:
+                break
+
+        return int(middle)
 
 
 def main():
-    print(Solution().mySqrt(4))
+    print(Solution().mySqrt(73))
 
 
 if __name__ == "__main__":
