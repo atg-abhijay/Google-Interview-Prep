2309cc636dc104a7e981d55ece05c872c59fd648
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-04 23:13:14 -0400

    649. Dota2 Senate: Initial commit

---
+++ Queue/dota2_senate_649.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/dota2-senate/
+"""
+
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        return ""
+
+
+def main():
+    print(Solution().predictPartyVictory("RDD"))
+
+
+if __name__ == "__main__":
+    main()
