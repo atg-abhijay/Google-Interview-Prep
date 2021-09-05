f796bbb34bd72a4a887ed8abd83228d135f456c6
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-05 16:21:32 -0400

    73. Set Matrix Zeroes: Initial commit

---
+++ Matrix/set_matrix_zeroes_73.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/set-matrix-zeroes/
+"""
+
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        return
+
+
+def main():
+    print(Solution().setZeroes([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]))
+
+
+if __name__ == "__main__":
+    main()
