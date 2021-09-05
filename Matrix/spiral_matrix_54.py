703253284bed3d031432fd91b58eca4bca0781e8
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-05 16:38:06 -0400

    54. Spiral Matrix: Initial commit

---
+++ Matrix/spiral_matrix_54.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/spiral-matrix/
+"""
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
+
+
+if __name__ == "__main__":
+    main()
