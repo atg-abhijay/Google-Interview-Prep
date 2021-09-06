9f59608e4200a5a4b3eb4e8f735f9818c738c115
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-05 23:57:21 -0400

    62. Unique Paths: Initial commit

---
+++ Graph/unique_paths_62.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/unique-paths/
+"""
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().uniquePaths(3, 7))
+
+
+if __name__ == "__main__":
+    main()
