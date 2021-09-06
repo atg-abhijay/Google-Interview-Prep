4a061405d3ef43c013d94a163166d98457723bca
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 00:32:28 -0400

    1643. Kth Smallest Instructions: Initial commit

---
+++ kth_smallest_instructions_1643.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/kth-smallest-instructions/
+"""
+
+
+class Solution(object):
+    def kthSmallestPath(self, destination, k):
+        """
+        :type destination: List[int]
+        :type k: int
+        :rtype: str
+        """
+        return ""
+
+
+def main():
+    print(Solution().kthSmallestPath([2, 3], 2))
+
+
+if __name__ == "__main__":
+    main()
