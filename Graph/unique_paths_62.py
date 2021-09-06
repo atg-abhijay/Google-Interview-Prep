1978f45dfa338db0a355edaa8ba745d31568bace
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 00:10:36 -0400

    Partially solve 62. Unique Paths (Time Limit Exceeded)

--- Graph/unique_paths_62.py
+++ Graph/unique_paths_62.py
@@ -11,7 +11,30 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        return -1
+        grid = [[0 for _ in range(n)] for _ in range(m)]
+        directions = [(0, 1), (1, 0)]
+        return self.performDFS([grid, directions], [0, 0], [m, n])
+
+
+    def performDFS(self, arrays, indices, constants):
+        grid, directions = arrays
+        row_idx, col_idx = indices
+        num_rows, num_cols = constants
+        if grid[row_idx][col_idx] == 1:
+            return 0
+
+        if row_idx == num_rows-1 and col_idx == num_cols-1:
+            return 1
+
+        num_paths = 0
+        grid[row_idx][col_idx] = 1
+        for incr_x, incr_y in directions:
+            nbr_row, nbr_col = row_idx + incr_x, col_idx + incr_y
+            if 0 <= nbr_row < len(grid) and 0 <= nbr_col < len(grid[0]):
+                num_paths += self.performDFS(arrays, [nbr_row, nbr_col], constants)
+                grid[nbr_row][nbr_col] = 0
+
+        return num_paths
 
 
 def main():
