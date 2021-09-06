5c7f7e148f1c4b02b3600857763a1f71e1912452
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 02:51:12 -0400

    1643 - Use DP instead of recursion
    Start from the bottom right and move in the opposite directions
    and progressively fill up/determine the paths. The next set of
    coordinates will use the previously calculated values (DP). This
    will make it a straight pass instead of recursing a lot.
    
    Still getting Time Limit Exceeded though

--- kth_smallest_instructions_1643.py
+++ kth_smallest_instructions_1643.py
@@ -5,6 +5,7 @@ https://leetcode.com/problems/kth-smallest-instructions/
 
 
 import heapq
+from collections import deque
 
 
 class StringWrapper:
@@ -29,32 +30,31 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        heap = []
         target_row, target_col = destination
-        directions = [(0, 1), (1, 0)]
-        grid = [[-1 for _ in range(target_col+1)] for _ in range(target_row+1)]
         grid_paths = [[[] for _ in range(target_col+1)] for _ in range(target_row+1)]
-        self.performDFS([0, 0], [heap, grid, grid_paths, directions], [target_row, target_col, k])
+        self.determinePaths(grid_paths, [target_row, target_col, k])
         return heapq.heappop(grid_paths[0][0]).val
 
 
-    def performDFS(self, indices, arrays, constants):
-        row_idx, col_idx = indices
-        heap, grid, grid_paths, directions = arrays
+    def determinePaths(self, grid_paths, constants):
         target_row, target_col, k = constants
+        backward_dirns = [(0, -1), (-1, 0)]
+        enqueued = [[0 for _ in range(target_col+1)] for _ in range(target_row+1)]
+        dq = deque([(target_row, target_col)])
+        grid_paths[target_row][target_col] = [StringWrapper("")]
+        while dq:
+            row_idx, col_idx = dq.popleft()
+            for decr_x, decr_y in backward_dirns:
+                nbr_row, nbr_col = row_idx + decr_x, col_idx + decr_y
+                within_bounds = 0 <= nbr_row <= target_row and 0 <= nbr_col <= target_col
+                if within_bounds:
+                    if not enqueued[nbr_row][nbr_col]:
+                        dq.append((nbr_row, nbr_col))
+                        enqueued[nbr_row][nbr_col] = 1
 
-        if row_idx == target_row and col_idx == target_col:
-            grid_paths[row_idx][col_idx] = [StringWrapper("")]
-
-        for incr_x, incr_y in directions:
-            nbr_row, nbr_col = row_idx + incr_x, col_idx + incr_y
-            if 0 <= nbr_row <= target_row and 0 <= nbr_col <= target_col:
-                if not grid_paths[nbr_row][nbr_col]:
-                    self.performDFS([nbr_row, nbr_col], arrays, constants)
-
-                dirn = "H" if incr_y == 1 else "V"
-                for path in grid_paths[nbr_row][nbr_col]:
-                    self.addPathForPosition(*indices, grid_paths, dirn, path, k)
+                    dirn = "H" if decr_y == -1 else "V"
+                    for path in grid_paths[row_idx][col_idx]:
+                        self.addPathForPosition(nbr_row, nbr_col, grid_paths, dirn, path, k)
 
 
     def addPathForPosition(self, row_idx, col_idx, grid_paths, dirn, path, k):
@@ -67,7 +67,7 @@ class Solution(object):
 
 
 def main():
-    print(Solution().kthSmallestPath([2, 3], 1))
+    print(Solution().kthSmallestPath([2, 3], 2))
 
 
 if __name__ == "__main__":
