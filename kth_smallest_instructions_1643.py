212dc4a12dbbc2feb86314aa516aec428acaa96e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 01:48:19 -0400

    1643 - Use heaps to store upto k smallest instructions
    Still getting Time Limit Exceeded

--- kth_smallest_instructions_1643.py
+++ kth_smallest_instructions_1643.py
@@ -4,6 +4,24 @@ https://leetcode.com/problems/kth-smallest-instructions/
 """
 
 
+import heapq
+
+
+class StringWrapper:
+    def __init__(self, value: str):
+        self.val = value
+
+    # Use a reverse comparison to make
+    # larger values smaller so that these
+    # larger values are popped from the heap,
+    # thereby only leaving the smaller values
+    def __lt__(self, other):
+        return other.val < self.val
+
+    def add_path(self, path):
+        self.val += path.val
+
+
 class Solution(object):
     def kthSmallestPath(self, destination, k):
         """
@@ -16,17 +34,17 @@ class Solution(object):
         directions = [(0, 1), (1, 0)]
         grid = [[-1 for _ in range(target_col+1)] for _ in range(target_row+1)]
         grid_paths = [[[] for _ in range(target_col+1)] for _ in range(target_row+1)]
-        self.performDFS([0, 0], [heap, grid, grid_paths, directions], [target_row, target_col])
-        return sorted(grid_paths[0][0])[k-1]
+        self.performDFS([0, 0], [heap, grid, grid_paths, directions], [target_row, target_col, k])
+        return heapq.heappop(grid_paths[0][0]).val
 
 
     def performDFS(self, indices, arrays, constants):
         row_idx, col_idx = indices
         heap, grid, grid_paths, directions = arrays
-        target_row, target_col = constants
+        target_row, target_col, k = constants
 
         if row_idx == target_row and col_idx == target_col:
-            grid_paths[row_idx][col_idx] = [""]
+            grid_paths[row_idx][col_idx] = [StringWrapper("")]
 
         for incr_x, incr_y in directions:
             nbr_row, nbr_col = row_idx + incr_x, col_idx + incr_y
@@ -36,7 +54,16 @@ class Solution(object):
 
                 dirn = "H" if incr_y == 1 else "V"
                 for path in grid_paths[nbr_row][nbr_col]:
-                    grid_paths[row_idx][col_idx].append(dirn + path)
+                    self.addPathForPosition(*indices, grid_paths, dirn, path, k)
+
+
+    def addPathForPosition(self, row_idx, col_idx, grid_paths, dirn, path, k):
+        new_path = StringWrapper(dirn)
+        new_path.add_path(path)
+        heap = grid_paths[row_idx][col_idx]
+        heapq.heappush(heap, new_path)
+        if len(heap) > k:
+            heapq.heappop(heap)
 
 
 def main():
