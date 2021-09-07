207d66f1d9c19fef74416f58b5929d56bae40b2b
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 20:26:13 -0400

    455. Assign Cookies: Initial commit

---
+++ Sorting/assign_cookies_455.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/assign-cookies/
+"""
+
+
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().findContentChildren(g=[1, 2, 3], s=[1, 1]))
+
+
+if __name__ == "__main__":
+    main()
