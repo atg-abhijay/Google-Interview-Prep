bcda83ea300dee5520e2d92685dbfe45dd9b2a59
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 23:05:22 -0400

    Solve 969. Pancake Sorting

--- Sorting/pancake_sorting_969.py
+++ Sorting/pancake_sorting_969.py
@@ -10,7 +10,29 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        return []
+        is_sorted, idx_next = True, 1
+        for num in arr[:-1]:
+            if num > arr[idx_next]:
+                is_sorted = False
+                break
+
+            idx_next += 1
+
+        if is_sorted:
+            return []
+
+        flips = []
+        for target in range(len(arr), 1, -1):
+            target_idx = arr.index(target)
+            if target_idx != 0:
+                arr[:target_idx+1] = arr[:target_idx+1][::-1]
+                flips.append(target_idx+1)
+
+            num = arr[0]
+            arr[:num] = arr[:num][::-1]
+            flips.append(num)
+
+        return flips
 
 
 def main():
