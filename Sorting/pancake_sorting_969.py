405272efb4977534ba2348ea27dc75427c74444f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 22:12:24 -0400

    969. Pancake Sorting: Initial commit

---
+++ Sorting/pancake_sorting_969.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/pancake-sorting/
+"""
+
+
+class Solution(object):
+    def pancakeSort(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().pancakeSort([3, 2, 4, 1]))
+
+
+if __name__ == "__main__":
+    main()
