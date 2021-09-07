c72dc51073748f09f6a905cfbf102fa4ed231069
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-07 00:32:51 -0400

    1029. Two City Scheduling: Initial commit

---
+++ Sorting/two_city_scheduling_1029.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/two-city-scheduling/
+"""
+
+
+class Solution(object):
+    def twoCitySchedCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().twoCitySchedCost([[10, 20], [30, 200], [400, 50], [30, 20]]))
+
+
+if __name__ == "__main__":
+    main()
