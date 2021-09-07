34d20ecb636f7c906982b1e82227cbd2fdbf7ac6
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-06 20:47:22 -0400

    Solve 455. Assign Cookies

--- Sorting/assign_cookies_455.py
+++ Sorting/assign_cookies_455.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/assign-cookies/
 """
 
 
+from bisect import bisect_left
+
+
 class Solution(object):
     def findContentChildren(self, g, s):
         """
@@ -11,11 +14,23 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
-        return -1
+        if not s:
+            return 0
+
+        content_children = 0
+        g.sort(), s.sort()
+        start_idx = 0
+        for greed in g:
+            size_idx = bisect_left(s, greed, lo=start_idx)
+            if 0 <= size_idx < len(s):
+                content_children += 1
+                start_idx = size_idx + 1
+
+        return content_children
 
 
 def main():
-    print(Solution().findContentChildren(g=[1, 2, 3], s=[1, 1]))
+    print(Solution().findContentChildren([10, 9, 8, 7], [3, 4, 5, 6]))
 
 
 if __name__ == "__main__":
