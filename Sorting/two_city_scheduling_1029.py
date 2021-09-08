0fe3ed4aebc0578495ae03c2c16312a162d8aab0
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-07 21:44:40 -0400

    Solve 1029. Two City Scheduling
    Super happy to have solved a DP problem all the way by myself!!!

--- Sorting/two_city_scheduling_1029.py
+++ Sorting/two_city_scheduling_1029.py
@@ -10,11 +10,35 @@ class Solution(object):
         :type costs: List[List[int]]
         :rtype: int
         """
-        return -1
+        n = int(len(costs) / 2)
+        min_costs = [[float("inf") for _ in range(n + 1)] for _ in range(n + 1)]
+        min_costs[0][0] = 0
+        for idx, [cost_a, cost_b] in enumerate(costs):
+            idx += 1
+            for row_idx, col_idx in zip(range(idx+1), range(idx, -1, -1)):
+                if not (0 <= row_idx <= n and 0 <= col_idx <= n):
+                    continue
+
+                north_x = row_idx - 1
+                lowest_value = min_costs[row_idx][col_idx]
+                if north_x >= 0:
+                    lowest_value = min(min_costs[north_x][col_idx] + cost_b, lowest_value)
+
+                west_y = col_idx - 1
+                if west_y >= 0:
+                    lowest_value = min(min_costs[row_idx][west_y] + cost_a, lowest_value)
+
+                min_costs[row_idx][col_idx] = lowest_value
+
+        return min_costs[n][n]
 
 
 def main():
-    print(Solution().twoCitySchedCost([[10, 20], [30, 200], [400, 50], [30, 20]]))
+    print(
+        Solution().twoCitySchedCost(
+            [[259, 770], [448, 54], [926, 667], [184, 139], [840, 118], [577, 469]]
+        )
+    )
 
 
 if __name__ == "__main__":
