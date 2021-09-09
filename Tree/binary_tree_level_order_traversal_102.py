ac538c92f8bf8ad9d52b75a28481bb5dd95e06a7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 02:36:01 -0400

    Solve 102. Binary Tree Level Order Traversal

--- Tree/binary_tree_level_order_traversal_102.py
+++ Tree/binary_tree_level_order_traversal_102.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/binary-tree-level-order-traversal/
 """
 
 
+from collections import deque
+
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
@@ -18,4 +21,44 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        return [[]]
+        if not root:
+            return []
+
+        queue, traversal = deque([root]), [[]]
+        currently_added, upper_bound = 0, 1
+        increment = 0
+        while queue:
+            node = queue.popleft()
+            if currently_added < upper_bound:
+                traversal[-1].append(node.val)
+                currently_added += 1
+            else:
+                traversal.append([node.val])
+                currently_added = 1
+                upper_bound = increment
+                increment = 0
+
+            if node.left:
+                queue.append(node.left)
+                increment += 1
+
+            if node.right:
+                queue.append(node.right)
+                increment += 1
+
+        return traversal
+
+
+def main():
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.left.left = TreeNode(13)
+    root.left.right = TreeNode(18)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(Solution().levelOrder(root))
+
+
+if __name__ == "__main__":
+    main()
