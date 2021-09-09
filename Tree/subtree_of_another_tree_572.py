22822263015bf95fe542ed25aac5af5849b0f386
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 19:55:53 -0400

    572. Subtree of Another Tree: Initial commit

---
+++ Tree/subtree_of_another_tree_572.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/subtree-of-another-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        return False
