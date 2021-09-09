b255b99f0f8e2341f96a4dda31c48cd23cabbdcd
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-08 22:59:55 -0400

    104. Maximum Depth of Binary Tree: Initial commit

---
+++ Tree/max_depth_binary_tree_104.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return -1
