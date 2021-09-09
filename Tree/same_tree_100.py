a9c44c116c9241edf90c4dd4571efb21166caaf5
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 00:29:42 -0400

    100. Same Tree: Initial commit

---
+++ Tree/same_tree_100.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/same-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        return False
