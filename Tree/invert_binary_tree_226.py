6ebad4d0dbbcfed24e4ae306fd1089cd5f3da7d3
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 01:06:18 -0400

    Solve 226. Invert Binary Tree

--- Tree/invert_binary_tree_226.py
+++ Tree/invert_binary_tree_226.py
@@ -18,4 +18,8 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        return None
+        if not root:
+            return None
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
