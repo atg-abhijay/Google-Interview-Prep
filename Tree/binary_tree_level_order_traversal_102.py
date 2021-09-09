28c182f2214ec99b9a68aa57cd2ace63d493d92d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 01:14:02 -0400

    102. Binary Tree Level Order Traversal: Initial commit

---
+++ Tree/binary_tree_level_order_traversal_102.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        return [[]]
