76e7542ec1c472793ae93b27c57f11b628f7abdd
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-08 23:26:21 -0400

    Solve 104. Maximum Depth of Binary Tree using iterative BFS

--- Tree/max_depth_binary_tree_104.py
+++ Tree/max_depth_binary_tree_104.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/
 """
 
 
+from collections import deque
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -19,5 +22,29 @@ class Solution:
 
         max_depth = 1
         max_depth += max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        return max_depth
+
+
+    def maxDepthIterativeBFS(self, root: TreeNode) -> int:
+        # Since the values of the nodes are not
+        # being used, store the depths in them
+        if not root:
+            return 0
+
+        nodes_deque = deque([root])
+        max_depth = 1
+        root.val = max_depth
+        while nodes_deque:
+            node = nodes_deque.popleft()
+            if node.left:
+                node.left.val = node.val + 1
+                nodes_deque.append(node.left)
+
+            if node.right:
+                node.right.val = node.val + 1
+                nodes_deque.append(node.right)
+
+            max_depth = max(max_depth, node.val)
 
         return max_depth
