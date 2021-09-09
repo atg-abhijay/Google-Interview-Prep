d0fa7c51096a241f27ec0f4b37425e196aa7eeef
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 00:53:11 -0400

    Solve 100. Same Tree

--- Tree/same_tree_100.py
+++ Tree/same_tree_100.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/same-tree/
 """
 
 
+from collections import deque
+
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
@@ -19,4 +22,30 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        return False
+        if not p and not q:
+            return True
+
+        if bool(p) ^ bool(q):
+            return False
+
+        p_nodes, q_nodes = deque([p]), deque([q])
+        while p_nodes and q_nodes:
+            node_p, node_q = p_nodes.popleft(), q_nodes.popleft()
+            if node_p.val != node_q.val:
+                return False
+
+            if bool(node_p.left) ^ bool(node_q.left):
+                return False
+
+            if node_p.left:
+                p_nodes.append(node_p.left)
+                q_nodes.append(node_q.left)
+
+            if bool(node_p.right) ^ bool(node_q.right):
+                return False
+
+            if node_p.right:
+                p_nodes.append(node_p.right)
+                q_nodes.append(node_q.right)
+
+        return True
