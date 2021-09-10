7a32834891e917eaf7d3b5564f22bf3d7889285e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 22:36:29 -0400

    235. Lowest Common Ancestor of a Binary Search Tree: Initial commit

---
+++ Tree/lowest_common_ancestor_in_bst_235.py
@@ -0,0 +1,23 @@
+"""
+URL of problem:
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        return None
