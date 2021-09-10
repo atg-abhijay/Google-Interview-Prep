bc83b2470bf6c4273ae341bb39db41336c09dea7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-09 22:24:27 -0400

    Solve 230. Kth Smallest Element in a BST

--- Tree/kth_smallest_element_bst_230.py
+++ Tree/kth_smallest_element_bst_230.py
@@ -4,6 +4,10 @@ https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 """
 
 
+from collections import deque
+import heapq
+
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
@@ -19,4 +23,29 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        return -1
+        TreeNode.__lt__ = lambda self, other: other.val < self.val
+        heap, num_nodes = [], 0
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            heapq.heappush(heap, node)
+            num_nodes += 1
+            if num_nodes > k:
+                heapq.heappop(heap)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        return heap[0].val
+
+
+def main():
+    root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6))
+    print(Solution().kthSmallest(root, 3))
+
+
+if __name__ == "__main__":
+    main()
