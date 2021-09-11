6e06593bd540d8d21c45fa00cd440781edac9943
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-10 22:55:47 -0400

    Solve 124. Binary Tree Maximum Path Sum

--- Tree/binary_tree_maximum_path_sum_124.py
+++ Tree/binary_tree_maximum_path_sum_124.py
@@ -18,4 +18,29 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        return -1
+        return self.determineMaxSum(root, dict())
+
+
+    def determineMaxSum(self, root, nodes_sum):
+        max_sum = float('-inf')
+        if not root.left and not root.right:
+            nodes_sum[root] = root.val
+            return root.val
+
+        left_value, right_value = 0, 0
+        if root.left:
+            max_sum = max(max_sum, self.determineMaxSum(root.left, nodes_sum))
+            left_value = nodes_sum[root.left]
+        if root.right:
+            max_sum = max(max_sum, self.determineMaxSum(root.right, nodes_sum))
+            right_value = nodes_sum[root.right]
+
+        nodes_sum[root] = max([
+            root.val, root.val + left_value, root.val + right_value
+        ])
+
+        max_sum = max([
+            max_sum, nodes_sum[root], root.val + left_value + right_value
+        ])
+
+        return max_sum
