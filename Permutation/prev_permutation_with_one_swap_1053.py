2ddf834aac87d43644b473e8c9f4d591a5ad87d2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-11 18:37:35 -0400

    1053. Previous Permutation With One Swap: Initial commit

---
+++ Permutation/prev_permutation_with_one_swap_1053.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/previous-permutation-with-one-swap/
+"""
+
+
+class Solution(object):
+    def prevPermOpt1(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().prevPermOpt1([1, 9, 4, 6, 7]))
+
+
+if __name__ == "__main__":
+    main()
