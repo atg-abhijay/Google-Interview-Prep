cc18fe9dacf90b527217730ae3436d7b16fdcc6f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-10 22:16:30 -0400

    124. Binary Tree Maximum Path Sum: Initial commit

---
+++ Tree/binary_tree_maximum_path_sum_124.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/binary-tree-maximum-path-sum/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return -1
