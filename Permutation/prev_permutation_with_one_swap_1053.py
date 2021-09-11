46079ed1a6a9f326ee2916ed4ab4ae6358a06aea
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-11 19:11:42 -0400

    Solve 1053. Previous Permutation With One Swap

--- Permutation/prev_permutation_with_one_swap_1053.py
+++ Permutation/prev_permutation_with_one_swap_1053.py
@@ -10,7 +10,27 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        return []
+        if arr == sorted(arr):
+            return arr
+
+        swap_idx_a, swap_idx_b = -1, -1
+        for idx in range(len(arr)-1, -1, -1):
+            current_num, prev_num = arr[idx], arr[idx-1]
+            if current_num >= prev_num:
+                continue
+
+            swap_idx_a = idx-1
+            greatest_min = 0
+            for sub_idx, num in enumerate(arr[idx:]):
+                if num >= prev_num:
+                    break
+
+                if num > greatest_min:
+                    greatest_min = num
+                    swap_idx_b = idx+sub_idx
+
+            arr[swap_idx_a], arr[swap_idx_b] = arr[swap_idx_b], arr[swap_idx_a]
+            return arr
 
 
 def main():
