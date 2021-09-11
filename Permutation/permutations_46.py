2f007cb8171e934a08a747e9b99f713365de39d7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-11 17:26:48 -0400

    46. Permutations: Initial commit

---
+++ Permutation/permutations_46.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/permutations/
+"""
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().permute([1, 2, 3]))
+
+
+if __name__ == "__main__":
+    main()
