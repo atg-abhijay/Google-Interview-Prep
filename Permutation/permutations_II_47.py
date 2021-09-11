1be65415bf53e12ab2073422a598e98138e5703c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-11 18:15:11 -0400

    47. Permutations II: Initial commit

---
+++ Permutation/permutations_II_47.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/permutations-ii/
+"""
+
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().permuteUnique([1, 1, 2]))
+
+
+if __name__ == "__main__":
+    main()
