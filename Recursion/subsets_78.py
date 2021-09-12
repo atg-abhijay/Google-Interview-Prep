d55e7ec9f55f1ac5301610ef5f97966a262ce4c2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-12 17:18:15 -0400

    78. Subsets: Initial commit

---
+++ Recursion/subsets_78.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/subsets/
+"""
+
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().subsets([1, 2, 3]))
+
+
+if __name__ == "__main__":
+    main()
