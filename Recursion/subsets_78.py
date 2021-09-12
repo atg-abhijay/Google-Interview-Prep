7648d9f0660fbd35fb00d74f62330438825cac34
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-12 17:34:19 -0400

    Solve 78. Subsets

--- Recursion/subsets_78.py
+++ Recursion/subsets_78.py
@@ -10,7 +10,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        return [[]]
+        if not nums:
+            return [[]]
+
+        subsets = []
+        sub_results = self.subsets(nums[1:])
+        subsets.extend(sub_results)
+        for sub_res in sub_results:
+            sub_res_copy = sub_res.copy()
+            sub_res_copy.append(nums[0])
+            subsets.append(sub_res_copy)
+
+        return subsets
 
 
 def main():
