3704c8c90bdc9f26de62c01def3dbaf053d72318
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-16 00:29:19 -0400

    1643 - Represent the paths using bits, stored as integer values
    If the move is "H", assign a bit of 0. If the move is "V", assign
    a bit of 1. We do this because H is lexicographically smaller
    than V and correspondingly, 0 is less than 1 for binary.
    e.g. A comparison between HHV and VHH becomes a comparison
    between 001 (1) and 100 (4) wherein 001 is the lesser value.
    
    Continue to wrap the path in an object since we need different
    comparison strategies depending on the value of k and because
    we can't change comparison strategies for integer values (the
    paths are now stored as integers).
    
    Take care of some binary stuff because leading 0s are ignored.
    Consider a variable steps_away which denotes the number of steps
    to reach the end from a cell located at (i, j). Use that to
    generate the path values for a cell at (i, j).

--- kth_smallest_instructions_1643.py
+++ kth_smallest_instructions_1643.py
@@ -9,12 +9,9 @@ from collections import deque
 from math import comb
 
 
-class StringWrapper:
-    def __init__(self, value: str):
-        self.val = [value]
-
-    def add_path(self, path):
-        self.val.extend(path.val)
+class PathWrapper:
+    def __init__(self, path:int):
+        self.val = path
 
 
 class Solution(object):
@@ -28,57 +25,64 @@ class Solution(object):
         num_combinations = comb(target_row + target_col, target_row)
         if k > int(num_combinations/2):
             k = num_combinations - k + 1
-            StringWrapper.__lt__ = lambda self, other: self.val < other.val
+            PathWrapper.__lt__ = lambda self, other: self.val < other.val
         else:
             # Use a reverse comparison to make
             # larger values smaller so that these
             # larger values are popped from the heap,
             # thereby only leaving the smaller values
-            StringWrapper.__lt__ = lambda self, other: other.val < self.val
+            PathWrapper.__lt__ = lambda self, other: other.val < self.val
 
         grid_paths = [[[] for _ in range(target_col+1)] for _ in range(target_row+1)]
         self.determinePaths(grid_paths, [target_row, target_col, k])
-        return ''.join(heapq.heappop(grid_paths[0][0]).val)
+        kth_path = heapq.heappop(grid_paths[0][0]).val
+        kth_path_bits = bin(kth_path)[2:].zfill(target_row+target_col)
+        return ''.join(["H" if bit == "0" else "V" for bit in kth_path_bits])
+        # str_kth_path = []
+        # for bit in str(kth_path.val):
+        #     str_kth_path.append("H" if bit == "0" else "V")
+
+        # return ''.join(str_kth_path)
+        # return ''.join(["H" for bit in str(kth_path) if bit == "0" else "V"])
+        # return ''.join(heapq.heappop(grid_paths[0][0]).val)
 
 
     def determinePaths(self, grid_paths, constants):
         target_row, target_col, k = constants
         backward_dirns = [(0, -1), (-1, 0)]
         enqueued = [[0 for _ in range(target_col+1)] for _ in range(target_row+1)]
-        dq = deque([(target_row, target_col)])
-        grid_paths[target_row][target_col] = [StringWrapper("")]
-        while dq:
-            row_idx, col_idx = dq.popleft()
+        queue = deque([(target_row, target_col)])
+        grid_paths[target_row][target_col] = [PathWrapper(0)]
+        # num_iterations = 0
+        while queue:
+            row_idx, col_idx = queue.popleft()
             if [row_idx, col_idx] == [0, 0]:
                 break
+            # num_iterations += 1
+            # print(num_iterations, (row_idx, col_idx))
             for decr_x, decr_y in backward_dirns:
                 nbr_row, nbr_col = row_idx + decr_x, col_idx + decr_y
                 within_bounds = 0 <= nbr_row <= target_row and 0 <= nbr_col <= target_col
-                if within_bounds:
-                    if not enqueued[nbr_row][nbr_col]:
-                        dq.append((nbr_row, nbr_col))
-                        enqueued[nbr_row][nbr_col] = 1
+                if not within_bounds:
+                    continue
 
-                    dirn = "H" if decr_y == -1 else "V"
-                    for path in grid_paths[row_idx][col_idx]:
-                        self.addPathForPosition(nbr_row, nbr_col, grid_paths, dirn, path, k)
+                if not enqueued[nbr_row][nbr_col]:
+                    queue.append((nbr_row, nbr_col))
+                    enqueued[nbr_row][nbr_col] = 1
 
-            # Clear out the paths since they
-            # have been added to the neighbors now
-            grid_paths[row_idx][col_idx] = []
-
-
-    def addPathForPosition(self, row_idx, col_idx, grid_paths, dirn, path, k):
-        new_path = StringWrapper(dirn)
-        new_path.add_path(path)
-        heap = grid_paths[row_idx][col_idx]
-        heapq.heappush(heap, new_path)
-        if len(heap) > k:
-            heapq.heappop(heap)
+                dirn = "H" if decr_y == -1 else "V"
+                steps_away = target_row + target_col - nbr_row - nbr_col - 1
+                for path in grid_paths[row_idx][col_idx]:
+                    nbr_path = 1 << steps_away if dirn == "V" else 0
+                    nbr_path |= path.val
+                    heap = grid_paths[nbr_row][nbr_col]
+                    heapq.heappush(heap, PathWrapper(nbr_path))
+                    if len(heap) > k:
+                        heapq.heappop(heap)
 
 
 def main():
-    print(Solution().kthSmallestPath([2, 3], 2))
+    print(Solution().kthSmallestPath([2, 3], 1))
 
 
 if __name__ == "__main__":
