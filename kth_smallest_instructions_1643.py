e91a33a6051a42e6884ac9afb2ff4b1747706efc
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-16 00:52:48 -0400

    1643 - Simplify things w.r.t the heap
    Instead of wrapping the integer path in an object so as to
    support different comparison strategies, use a "weight" variable
    which is set to -1 if we want a max heap, or set to 1 if we want
    a min heap. A min heap is needed when k is greater than half the
    number of possible combinations.
    
    When pushing into the heap, multiply the integer path with the
    weight so that the product will serve as the key/comparison value
    when pushing and popping from the heap.
    
    The key value for the target cell doesn't matter since no other
    cell can get to it using the current traversal strategy and
    since the target cell is the starting point anyways - assign the
    product to be 0.

--- kth_smallest_instructions_1643.py
+++ kth_smallest_instructions_1643.py
@@ -9,11 +9,6 @@ from collections import deque
 from math import comb
 
 
-class PathWrapper:
-    def __init__(self, path:int):
-        self.val = path
-
-
 class Solution(object):
     def kthSmallestPath(self, destination, k):
         """
@@ -23,43 +18,31 @@ class Solution(object):
         """
         target_row, target_col = destination
         num_combinations = comb(target_row + target_col, target_row)
+        # When using the heap, use a weight of -1 to make larger
+        # values smaller so that these larger values are popped
+        # from the heap, thereby only leaving the smaller values
+        weight = -1
         if k > int(num_combinations/2):
             k = num_combinations - k + 1
-            PathWrapper.__lt__ = lambda self, other: self.val < other.val
-        else:
-            # Use a reverse comparison to make
-            # larger values smaller so that these
-            # larger values are popped from the heap,
-            # thereby only leaving the smaller values
-            PathWrapper.__lt__ = lambda self, other: other.val < self.val
+            weight = 1
 
-        grid_paths = [[[] for _ in range(target_col+1)] for _ in range(target_row+1)]
-        self.determinePaths(grid_paths, [target_row, target_col, k])
-        kth_path = heapq.heappop(grid_paths[0][0]).val
+        kth_path = self.determinePaths([target_row, target_col, k], weight)
         kth_path_bits = bin(kth_path)[2:].zfill(target_row+target_col)
         return ''.join(["H" if bit == "0" else "V" for bit in kth_path_bits])
-        # str_kth_path = []
-        # for bit in str(kth_path.val):
-        #     str_kth_path.append("H" if bit == "0" else "V")
-
-        # return ''.join(str_kth_path)
-        # return ''.join(["H" for bit in str(kth_path) if bit == "0" else "V"])
-        # return ''.join(heapq.heappop(grid_paths[0][0]).val)
 
 
-    def determinePaths(self, grid_paths, constants):
+    def determinePaths(self, constants, weight):
         target_row, target_col, k = constants
         backward_dirns = [(0, -1), (-1, 0)]
+        grid_paths = [[[] for _ in range(target_col+1)] for _ in range(target_row+1)]
         enqueued = [[0 for _ in range(target_col+1)] for _ in range(target_row+1)]
         queue = deque([(target_row, target_col)])
-        grid_paths[target_row][target_col] = [PathWrapper(0)]
-        # num_iterations = 0
+        grid_paths[target_row][target_col] = [(0, 0)]
         while queue:
             row_idx, col_idx = queue.popleft()
             if [row_idx, col_idx] == [0, 0]:
                 break
-            # num_iterations += 1
-            # print(num_iterations, (row_idx, col_idx))
+
             for decr_x, decr_y in backward_dirns:
                 nbr_row, nbr_col = row_idx + decr_x, col_idx + decr_y
                 within_bounds = 0 <= nbr_row <= target_row and 0 <= nbr_col <= target_col
@@ -71,14 +54,15 @@ class Solution(object):
                     enqueued[nbr_row][nbr_col] = 1
 
                 dirn = "H" if decr_y == -1 else "V"
+                heap = grid_paths[nbr_row][nbr_col]
                 steps_away = target_row + target_col - nbr_row - nbr_col - 1
                 for path in grid_paths[row_idx][col_idx]:
-                    nbr_path = 1 << steps_away if dirn == "V" else 0
-                    nbr_path |= path.val
-                    heap = grid_paths[nbr_row][nbr_col]
-                    heapq.heappush(heap, PathWrapper(nbr_path))
+                    nbr_path = (1 << steps_away if dirn == "V" else 0) | path[1]
+                    heapq.heappush(heap, (weight * nbr_path, nbr_path))
                     if len(heap) > k:
                         heapq.heappop(heap)
+
+        return heapq.heappop(grid_paths[0][0])[1]
 
 
 def main():
