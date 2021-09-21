ca9e3c76e859088975d37dcc45ff2925a767093e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-09-21 15:53:47 -0400

    33. Search in Rotated Sorted Array: Initial commit

---
+++ search_rotated_sorted_array_33.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+"""
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().search([4, 5, 6, 7, 0, 1, 2], 0))
+
+
+if __name__ == "__main__":
+    main()
