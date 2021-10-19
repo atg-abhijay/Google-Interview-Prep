c5b153de522aba1a49bd94e83f03e95cf2cc8de1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-18 23:08:00 -0400

    Solve 53. Maximum Subarray*

--- Array/maximum_subarray_53.py
+++ Array/maximum_subarray_53.py
@@ -24,7 +24,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return -1
+        current_sum, max_sum = nums[0], nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, num + current_sum)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
 
 
 def main():
