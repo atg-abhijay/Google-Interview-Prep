cb951f85d0a896e6dc2c09870dc5040ad7b1dd0c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-24 17:09:23 -0400

    268. Missing Number: Move solutions into one file*

--- Binary/missing_number_268.py
+++ Binary/missing_number_268.py
@@ -8,7 +8,7 @@ from functools import reduce
 
 
 class Solution(object):
-    def missingNumber(self, nums):
+    def missingNumber_binary(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -24,7 +24,32 @@ class Solution(object):
         return xor_value
 
 
-    def missingNumber_2ndPass(self, nums):
+    def missingNumber_arrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        all_nums = [0] * (len(nums)+1)
+        for num in nums:
+            all_nums[num] = 1
+
+        for idx, num_found in enumerate(all_nums):
+            if not num_found:
+                return idx
+
+
+    def missingNumber_sums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_values = len(nums)
+        total_sum = int((num_values * (num_values + 1)) / 2)
+        given_vals_sum = sum(nums)
+        return total_sum - given_vals_sum
+
+
+    def missingNumber_binary_2ndPass(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -34,8 +59,26 @@ class Solution(object):
         return reduce(lambda x, y: x ^ y, all_nums)
 
 
+    def missingNumber_sums_2ndPass(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_elems = len(nums)
+        sum_upto_n = num_elems * (num_elems + 1) // 2
+        return sum_upto_n - sum(nums)
+
+
+    def missingNumber_sets_2ndPass(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return set(range(len(nums) + 1)).difference(set(nums)).pop()
+
+
 def main():
-    print(Solution().missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+    print(Solution().missingNumber_binary([9, 6, 4, 2, 3, 5, 7, 0, 1]))
 
 
 if __name__ == "__main__":
