9153375c755e7ed55b6dc8d6895b06576bf42163
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-23 22:18:24 -0400

    Solve 33. Search in Rotated Sorted Array

--- Array/search_rotated_sorted_array_33.py
+++ Array/search_rotated_sorted_array_33.py
@@ -11,7 +11,27 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        return -1
+        # Add a dummy value at the end to be able
+        # to return the last index if that is the answer
+        nums.append(float('inf'))
+        start_idx, stop_idx = 0, len(nums) - 1
+        while 1:
+            mid_idx = (start_idx + stop_idx) // 2
+            start_val, mid_val = nums[start_idx], nums[mid_idx]
+            if start_val < mid_val:
+                if start_val <= target < mid_val:
+                    stop_idx = mid_idx
+                else:
+                    start_idx = mid_idx
+
+            elif start_val > mid_val:
+                if target >= start_val or target < mid_val:
+                    stop_idx = mid_idx
+                else:
+                    start_idx = mid_idx
+
+            else:
+                return start_idx if target == start_val else -1
 
 
 def main():
