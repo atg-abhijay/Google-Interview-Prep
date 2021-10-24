a50a49bf4e405aea3b3157e9154cae4bc941dfc2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-23 22:28:29 -0400

    11. Container With Most Water: Initial commit

---
+++ Array/container_most_water_11.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/container-with-most-water/
+"""
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+
+
+if __name__ == "__main__":
+    main()
