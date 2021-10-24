9f4d38d52f91747b5bfcb49aedec1e046cd271f1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-24 18:03:57 -0400

    371. Sum of Two Integers: Initial commit

---
+++ Binary/sum_two_integers_371.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/sum-of-two-integers/
+"""
+
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().getSum(2, 4))
+
+
+if __name__ == "__main__":
+    main()
