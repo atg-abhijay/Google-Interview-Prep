72114bd6eb590ea19f44a174ca7e3187beb4f225
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-25 19:25:45 -0400

    1232. Check If It Is a Straight Line: Initial commit

---
+++ Geometry/check_if_straight_line_1232.py
@@ -0,0 +1,23 @@
+"""
+URL of problem:
+https://leetcode.com/problems/check-if-it-is-a-straight-line/
+"""
+
+
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(
+        Solution().checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]])
+    )
+
+
+if __name__ == "__main__":
+    main()
