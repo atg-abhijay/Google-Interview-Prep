da286ad90a08da8448da84b230c2cfa1b71f612c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-25 22:02:22 -0400

    858. Mirror Reflection: Initial commit

---
+++ Geometry/mirror_reflection_858.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/mirror-reflection/
+"""
+
+
+class Solution(object):
+    def mirrorReflection(self, p, q):
+        """
+        :type p: int
+        :type q: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().mirrorReflection(3, 1))
+
+
+if __name__ == "__main__":
+    main()
