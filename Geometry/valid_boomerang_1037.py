81c551b6c8a8c368ac3c4d03ff7e1d637beb8f8f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-25 21:51:26 -0400

    1037. Valid Boomerang: Initial commit

---
+++ Geometry/valid_boomerang_1037.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/valid-boomerang/
+"""
+
+
+class Solution(object):
+    def isBoomerang(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().isBoomerang([[1, 1], [2, 3], [3, 2]]))
+
+
+if __name__ == "__main__":
+    main()
