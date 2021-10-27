eb6a4aac9487c61210ed988419afb6cb216ebd0f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-26 23:12:19 -0400

    Solve 1828. Queries on Number of Points Inside a Circle using sorting*
    Learnt this approach from the LeetCode discussion here -
    https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/discuss/1165522/C%2B%2B-Brute-Force-vs.-Sort

--- Geometry/queries_points_inside_circle_1828.py
+++ Geometry/queries_points_inside_circle_1828.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/
 """
 
 
+import bisect
+
+
 class Solution(object):
     def countPoints(self, points, queries):
         """
@@ -37,6 +40,32 @@ class Solution(object):
             num_inside = 0
             for x, y in points:
                 if (center_x - x) ** 2 + (center_y - y) ** 2 <= radius ** 2:
+                    num_inside += 1
+
+            answer.append(num_inside)
+
+        return answer
+
+
+    def countPoints_sort_2ndPass(self, points, queries):
+        """
+        :type points: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        answer = []
+        points.sort(key=lambda pt: pt[0])
+        x_coords = [point[0] for point in points]
+        for center_x, center_y, radius in queries:
+            start_idx = bisect.bisect_left(x_coords, center_x-radius)
+            # The index returned using bisect_right will be one AHEAD
+            # of the last occurence of center_x + radius. Therefore,
+            # we slice the list UP TO stop_idx so that the value
+            # at stop_idx itself isn't considered in the loop.
+            stop_idx = bisect.bisect_right(x_coords, center_x+radius)
+            num_inside, boundary = 0, radius ** 2
+            for x, y in points[start_idx:stop_idx]:
+                if (center_x - x) ** 2 + (center_y - y) ** 2 <= boundary:
                     num_inside += 1
 
             answer.append(num_inside)
