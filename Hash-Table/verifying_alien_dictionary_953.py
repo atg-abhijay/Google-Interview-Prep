67c1b85c3d368e146e507d64b466db915d0ae7e7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-28 23:14:24 -0400

    953 - Add time, space complexities, tags for topics and comments

--- Hash-Table/verifying_alien_dictionary_953.py
+++ Hash-Table/verifying_alien_dictionary_953.py
@@ -11,17 +11,28 @@ class Solution(object):
         :type order: str
         :rtype: bool
         """
+        # Time: O(nm)(?), n for size of 'words'
+        #       and m for average size of a word
+        # Space: O(1), since there are 26 letters
+        # Tags: Hash tables
         alphabets = {letter: idx for idx, letter in enumerate(order)}
         for f_word, s_word in zip(words, words[1:]):
             is_subset_word = True
             for f_char, s_char in zip(f_word, s_word):
+                # First word comes before second word
+                # - correct lexicographical order
                 if alphabets[f_char] < alphabets[s_char]:
                     is_subset_word = False
                     break
 
+                # First word comes after second word
+                # - wrong lexicographical order
                 if alphabets[f_char] > alphabets[s_char]:
                     return False
 
+            # The words are same up till the end of the
+            # shorter word. If first word is longer after
+            # that - wrong lexicographical order
             if is_subset_word and len(f_word) > len(s_word):
                 return False
 
