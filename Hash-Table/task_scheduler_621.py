4e1a5bb59dbe36751e19298604c7094a32320bec
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-29 00:02:50 -0400

    621. Task Scheduler: Initial commit

---
+++ Hash-Table/task_scheduler_621.py
@@ -0,0 +1,26 @@
+"""
+URL of problem:
+https://leetcode.com/problems/task-scheduler/
+"""
+
+
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(
+        Solution().leastInterval(
+            ["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
