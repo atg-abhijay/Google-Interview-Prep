4f91c97c57daa623e760ab2250e01fa10bda95e7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-28 23:46:11 -0400

    Solve 811. Subdomain Visit Count

--- Hash-Table/subdomain_visit_count_811.py
+++ Hash-Table/subdomain_visit_count_811.py
@@ -10,7 +10,21 @@ class Solution(object):
         :type cpdomains: List[str]
         :rtype: List[str]
         """
-        return []
+        counts_dict = {}
+        for cpdomain in cpdomains:
+            count, domain = cpdomain.split(" ")
+            split_subs = domain.split(".")
+            for subdomains in [split_subs, split_subs[1:], split_subs[2:]]:
+                if not subdomains:
+                    continue
+
+                subdomain = '.'.join(subdomains)
+                if subdomain not in counts_dict:
+                    counts_dict[subdomain] = int(count)
+                else:
+                    counts_dict[subdomain] += int(count)
+
+        return [f"{count} {subdomain}" for subdomain, count in counts_dict.items()]
 
 
 def main():
