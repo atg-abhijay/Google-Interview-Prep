517456504bbfea40d05ff7878216e8c16e9f6409
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-28 23:05:35 -0400

    Solve 953. Verifying an Alien Dictionary

--- Hash-Table/verifying_alien_dictionary_953.py
+++ Hash-Table/verifying_alien_dictionary_953.py
@@ -11,7 +11,21 @@ class Solution(object):
         :type order: str
         :rtype: bool
         """
-        return False
+        alphabets = {letter: idx for idx, letter in enumerate(order)}
+        for f_word, s_word in zip(words, words[1:]):
+            is_subset_word = True
+            for f_char, s_char in zip(f_word, s_word):
+                if alphabets[f_char] < alphabets[s_char]:
+                    is_subset_word = False
+                    break
+
+                if alphabets[f_char] > alphabets[s_char]:
+                    return False
+
+            if is_subset_word and len(f_word) > len(s_word):
+                return False
+
+        return True
 
 
 def main():
