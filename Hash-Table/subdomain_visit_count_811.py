8f38eb1f301c475fa3602f6f8fca9e7c7dbdf80c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-28 23:32:11 -0400

    811. Subdomain Visit Count: Initial commit

---
+++ Hash-Table/subdomain_visit_count_811.py
@@ -0,0 +1,25 @@
+"""
+URL of problem:
+https://leetcode.com/problems/subdomain-visit-count/
+"""
+
+
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        return []
+
+
+def main():
+    print(
+        Solution().subdomainVisits(
+            ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
