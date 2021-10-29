0d9a0f5db4ba03cb32eabc028b20e923863e0165
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-28 22:50:43 -0400

    953. Verifying an Alien Dictionary: Initial commit

---
+++ Hash-Table/verifying_alien_dictionary_953.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/verifying-an-alien-dictionary/
+"""
+
+
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz"))
+
+
+if __name__ == "__main__":
+    main()
