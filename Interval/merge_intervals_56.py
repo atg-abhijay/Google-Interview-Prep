dc5851e1f791136b557b83dfe6b60075c023b0d7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-30 18:19:04 -0400

    Partially solve 56. Merge Intervals

--- Interval/merge_intervals_56.py
+++ Interval/merge_intervals_56.py
@@ -33,11 +33,68 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        return [[]]
+        # Half-working solution
+        intervals.sort(key=lambda x: x[0])
+        if len(intervals) == 1:
+            return intervals
+
+        ans, overlapping_intvl = [], intervals[0]
+        did_overlap_occur = False
+        for idx, intvl in enumerate(intervals):
+            if self.is_overlap_2ndPass(intvl, overlapping_intvl):
+                overlapping_intvl = self.merge_two_intvls_2ndPass(intvl, overlapping_intvl)
+                did_overlap_occur = True
+            else:
+                if did_overlap_occur:
+                    ans.append(overlapping_intvl)
+                    did_overlap_occur = False
+                    overlapping_intvl = intvl
+
+                else:
+                    ans.append(intvl)
+
+        if did_overlap_occur:
+            ans.append(overlapping_intvl)
+
+        return ans
+
+
+        while 1:
+            ans, overlapping_intvl = [], intervals[0]
+            did_overlap_occur, did_update = False, False
+            for idx, intvl in enumerate(intervals[1:]):
+                if self.is_overlap_2ndPass(intvl, overlapping_intvl):
+                    overlapping_intvl = self.merge_two_intvls_2ndPass(intvl, overlapping_intvl)
+                    did_overlap_occur = True
+                else:
+                    if did_overlap_occur:
+                        ans.append(overlapping_intvl)
+                        ans.extend(intervals[idx+1:])
+                        did_update = True
+                        break
+
+                    ans.append(intvl)
+                    overlapping_intvl = intvl
+
+            if not did_overlap_occur:
+                return intervals
+
+            if not did_update:
+                ans.append(overlapping_intvl)
+
+            intervals = ans
+
+
+    def is_overlap_2ndPass(self, intvl_a, intvl_b):
+        return intvl_a[0] <= intvl_b[1] and intvl_b[0] <= intvl_a[1]
+
+
+    def merge_two_intvls_2ndPass(self, intvl_a, intvl_b):
+        return [min(intvl_a[0], intvl_b[0]), max(intvl_a[1], intvl_b[1])]
 
 
 def main():
-    print(Solution().merge([[4,5],[2,4],[4,6],[3,4],[0,0],[1,1],[3,5],[2,2]]))
+    print(Solution().merge_2ndPass([[1,3],[2,6],[8,10],[15,18]]))
 
 
 if __name__ == "__main__":
