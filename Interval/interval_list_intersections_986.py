9b6c7d8b20730295df12370457122248c8673748
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-10-31 00:04:16 -0400

    Partially solve 986. Interval List Intersections

--- Interval/interval_list_intersections_986.py
+++ Interval/interval_list_intersections_986.py
@@ -11,7 +11,43 @@ class Solution(object):
         :type secondList: List[List[int]]
         :rtype: List[List[int]]
         """
-        return [[]]
+        if bool(firstList) ^ bool(secondList):
+            return []
+
+        f_idx, s_idx = 0, 0
+        f_len, s_len = len(firstList), len(secondList)
+        ans = []
+        # for f_intvl in firstList:
+        #     for idx, s_intvl in enumerate(secondList[s_idx:]):
+        #         if self.is_overlap(f_intvl, s_intvl):
+        #             ans.append(self.find_intxn(f_intvl, s_intvl))
+        #         else:
+        #             s_idx += idx - 1
+
+        did_reset = False
+        while f_idx < f_len and s_idx < s_len:
+            f_intvl, s_intvl = firstList[f_idx], secondList[s_idx]
+            if self.is_overlap(f_intvl, s_intvl):
+                ans.append(self.find_intxn(f_intvl, s_intvl))
+                s_idx += 1
+                did_reset = False
+            else:
+                if not did_reset:
+                    s_idx -= 1
+                    f_idx += 1
+                    did_reset = True
+                else:
+                    s_idx += 1
+
+        return ans
+
+
+    def is_overlap(self, intvl_a, intvl_b):
+        return intvl_a[0] <= intvl_b[1] and intvl_b[0] <= intvl_a[1]
+
+
+    def find_intxn(self, intvl_a, intvl_b):
+        return [max(intvl_a[0], intvl_b[0]), min(intvl_a[1], intvl_b[1])]
 
 
 def main():
