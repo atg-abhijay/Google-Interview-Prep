300cb6342530bff256f8b3e9412b07656af671dd
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-02 22:56:16 -0400

    1249. Minimum Remove to Make Valid Parentheses: Initial commit

---
+++ Stack/min_remove_to_make_valid_parentheses_1249.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+"""
+
+
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ""
+
+
+def main():
+    print(Solution().minRemoveToMakeValid("lee(t(c)o)de)"))
+
+
+if __name__ == "__main__":
+    main()
