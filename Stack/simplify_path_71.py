ae37906250e9a574fdd2bafd7d6e07e5d0a1401a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-03 23:53:53 -0400

    71. Simplify Path: Initial commit

---
+++ Stack/simplify_path_71.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/simplify-path/
+"""
+
+
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        return ""
+
+
+def main():
+    print(Solution().simplifyPath("/a/./b/../../c/"))
+
+
+if __name__ == "__main__":
+    main()
