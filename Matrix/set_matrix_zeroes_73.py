f4ae8118323b6243202e903ba5b57c2fb5edd57a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-04 22:34:21 -0400

    73. Set Matrix Zeroes: Initial commit*

--- Matrix/set_matrix_zeroes_73.py
+++ Matrix/set_matrix_zeroes_73.py
@@ -33,6 +33,14 @@ class Solution(object):
         return
 
 
+    def setZeroes_2ndPass(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        return
+
+
 def main():
     print(Solution().setZeroes([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]))
 
