712dc68761012593b340cefac250b6ce7d873c80
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-05 17:08:01 -0400

    Solve 54. Spiral Matrix*

--- Matrix/spiral_matrix_54.py
+++ Matrix/spiral_matrix_54.py
@@ -43,11 +43,48 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        return []
+        if not matrix or not any(matrix):
+            return []
+
+        num_rows, num_cols = len(matrix), len(matrix[0])
+        if num_rows == 1:
+            return matrix[0]
+
+        if num_cols == 1:
+            return [row[0] for row in matrix]
+
+        row_start, row_end = 0, num_rows - 1
+        col_start, col_end = 0, num_cols - 1
+
+        top_row = matrix[0][col_start:col_end]
+        bottom_row = matrix[row_end][col_end:col_start:-1]
+        right_col = [row[col_end] for row in matrix[row_start:row_end]]
+        left_col = [row[col_start] for row in matrix[row_end:row_start:-1]]
+
+        spiral_list = [*top_row, *right_col, *bottom_row, *left_col]
+        matrix = [row[col_start+1:col_end] for row in matrix[row_start+1:row_end]]
+        spiral_list.extend(self.spiralOrder_2ndPass(matrix))
+
+        return spiral_list
 
 
 def main():
-    print(Solution().spiralOrder([[1,2],[3,4]]))
+    print(
+        Solution().spiralOrder_2ndPass(
+            [
+                [1, 11],
+                [2, 12],
+                [3, 13],
+                [4, 14],
+                [5, 15],
+                [6, 16],
+                [7, 17],
+                [8, 18],
+                [9, 19],
+                [10, 20],
+            ]
+        )
+    )
 
 
 if __name__ == "__main__":
