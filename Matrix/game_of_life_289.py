59575674648c517def443d89e60d7c43aea9a5f1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-05 17:45:46 -0400

    289. Game of Life: Initial commit

---
+++ Matrix/game_of_life_289.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/game-of-life/
+"""
+
+
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        return
+
+
+def main():
+    print(Solution().gameOfLife([[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]))
+
+
+if __name__ == "__main__":
+    main()
