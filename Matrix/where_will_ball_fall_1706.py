86f2a31dcc29b61ee0a2b508a3bee9ae24b38ec2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-05 23:15:08 -0400

    Solve 1706. Where Will the Ball Fall

--- Matrix/where_will_ball_fall_1706.py
+++ Matrix/where_will_ball_fall_1706.py
@@ -10,7 +10,33 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: List[int]
         """
-        return []
+        num_rows, num_cols = len(grid), len(grid[0])
+        ans = [-1] * num_cols
+        for col_idx in range(num_cols):
+            curr_row, curr_col = 0, col_idx
+            while curr_row < num_rows:
+                incline = grid[curr_row][curr_col]
+                nbr_incline = 0
+                if incline == 1 and curr_col + 1 < num_cols:
+                    nbr_incline = grid[curr_row][curr_col + 1]
+
+                if incline == -1 and curr_col - 1 >= 0:
+                    nbr_incline = grid[curr_row][curr_col - 1]
+
+                # If ball lands against the wall or
+                # it gets stuck in a "V"-shaped pattern
+                if nbr_incline == 0 or incline != nbr_incline:
+                    ans[col_idx] = -1
+                    break
+
+                curr_row += 1
+                curr_col += incline
+
+            # If the ball falls out at the bottom
+            if curr_row == num_rows:
+                ans[col_idx] = curr_col
+
+        return ans
 
 
 def main():
