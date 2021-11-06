79a25dc672ac0fdef095d5d95a7154271e482c58
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-06 19:53:44 -0400

    Solve 1352. Product of the Last K Numbers

--- Queue/product_of_last_k_numbers_1352.py
+++ Queue/product_of_last_k_numbers_1352.py
@@ -4,24 +4,52 @@ https://leetcode.com/problems/product-of-the-last-k-numbers/
 """
 
 
+import bisect
+
+
 class ProductOfNumbers(object):
     def __init__(self):
         self.stream = []
+        self.products = []
+        self.zero_idxs = []
+        self.size = 0
+        self.num_zeroes = 0
 
     def add(self, num):
         """
         :type num: int
         :rtype: None
         """
-        self.stream = [num * x for x in self.stream]
         self.stream.append(num)
+        self.size += 1
+        if num == 0:
+            self.zero_idxs.append(self.size - 1)
+            self.num_zeroes += 1
+            self.products.append(0)
+        else:
+            if self.products and self.products[-1] != 0:
+                self.products.append(num * self.products[-1])
+            else:
+                self.products.append(num)
 
     def getProduct(self, k):
         """
         :type k: int
         :rtype: int
         """
-        return self.stream[-1 * k]
+        start_idx = self.size - k
+        if self.find_index(start_idx):
+            return 0
+
+        return self.products[-1] // self.products[start_idx] * self.stream[start_idx]
+
+
+    def find_index(self, start_idx):
+        index = bisect.bisect_left(self.zero_idxs, start_idx)
+        if index < self.num_zeroes:
+            return True
+
+        return False
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
