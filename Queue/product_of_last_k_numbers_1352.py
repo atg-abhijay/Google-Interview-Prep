f6ecea7ffc0a620b8edae5948d92057bd7209158
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-06 17:06:02 -0400

    1352. Product of the Last K Numbers: Initial commit

---
+++ Queue/product_of_last_k_numbers_1352.py
@@ -0,0 +1,29 @@
+"""
+URL of problem:
+https://leetcode.com/problems/product-of-the-last-k-numbers/
+"""
+
+
+class ProductOfNumbers(object):
+    def __init__(self):
+        pass
+
+    def add(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        return
+
+    def getProduct(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        return -1
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
