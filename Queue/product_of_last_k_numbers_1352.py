c699a98a52164576eabd7788e96cde96eedfc44a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-06 22:05:22 -0400

    1352 - Further refactor code
    Only keep track of the latest index of '0' encountered. If the
    product in question uses an index that comes before the index
    for the zero, the whole product will be 0.

--- Queue/product_of_last_k_numbers_1352.py
+++ Queue/product_of_last_k_numbers_1352.py
@@ -4,16 +4,12 @@ https://leetcode.com/problems/product-of-the-last-k-numbers/
 """
 
 
-from bisect import bisect_left
-
-
 class ProductOfNumbers(object):
     def __init__(self):
         self.stream = []
         self.products = []
-        self.zero_idxs = []
+        self.last_zero_idx = -1
         self.size = 0
-        self.num_zeroes = 0
 
     def add(self, num):
         """
@@ -23,8 +19,7 @@ class ProductOfNumbers(object):
         self.stream.append(num)
         self.size += 1
         if num == 0:
-            self.zero_idxs.append(self.size - 1)
-            self.num_zeroes += 1
+            self.last_zero_idx = self.size - 1
             self.products.append(0)
         else:
             if self.products and self.products[-1] != 0:
@@ -38,7 +33,7 @@ class ProductOfNumbers(object):
         :rtype: int
         """
         start_idx = self.size - k
-        if bisect_left(self.zero_idxs, start_idx) < self.num_zeroes:
+        if start_idx <= self.last_zero_idx:
             return 0
 
         return self.products[-1] // self.products[start_idx] * self.stream[start_idx]
