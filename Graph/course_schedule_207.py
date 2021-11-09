197870a5d802812560d17fb8b8f41dd6e37a02fc
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-09 00:46:05 -0500

    207 - Add comments*

--- Graph/course_schedule_207.py
+++ Graph/course_schedule_207.py
@@ -61,6 +61,7 @@ class Solution(object):
         if numCourses == 1 or not prerequisites:
             return True
 
+        # Build an adjacency list representation of the graph
         courses = defaultdict(list)
         for crse, prereq in prerequisites:
             courses[crse].append(prereq)
@@ -68,6 +69,20 @@ class Solution(object):
         # 0 = not visited, 1 = in progress, 2 = completed
         visited = [0] * numCourses
         course_keys = set(courses.keys())
+        # Build a solution based on DFSing into the prerequisites graph.
+        # Two while loops to handle the case of a disconnected graph.
+        # - If status of a course is 0, add it's prerequisites to the
+        #   stack and change status to 1. Don't pop the course yet. We
+        #   need to maintain the stack to build a path in the graph to
+        #   be able to check for cycles.
+        # - If status of a course is 1, check if all of it's prerequisites
+        #   have been "completed" (status 2), meaning that they didn't
+        #   yield any cycles. If that happens, change the status to 2 and
+        #   pop the course. If all the prerequisites don't have status 2,
+        #   that means that a cycle has been encountered and the courses
+        #   cannot be finished.
+        # - If status of a course is 2, it means that that course can be completed
+        #   (it will not encounter any cycles). No need to traverse that course.
         while course_keys:
             stack = [course_keys.pop()]
             while stack:
