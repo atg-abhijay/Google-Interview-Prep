f2bc77323ac108e8a7faa6aa79c3a2f300dff01a
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-09 19:40:59 -0500

    128. Longest Consecutive Sequence: Initial commit

---
+++ Graph/longest_consecutive_sequence_128.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/longest-consecutive-sequence/
+"""
+
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+
+
+if __name__ == "__main__":
+    main()
