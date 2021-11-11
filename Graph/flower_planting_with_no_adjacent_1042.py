33621fa5daeaa04138a86c6284709ed3c2a51d26
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-11 17:55:07 -0500

    1042. Flower Planting With No Adjacent: Initial commit

---
+++ Graph/flower_planting_with_no_adjacent_1042.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/flower-planting-with-no-adjacent/
+"""
+
+
+class Solution(object):
+    def gardenNoAdj(self, n, paths):
+        """
+        :type n: int
+        :type paths: List[List[int]]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().gardenNoAdj(4, [[1, 2], [2, 3], [3, 4], [4, 1], [1, 3], [2, 4]]))
+
+
+if __name__ == "__main__":
+    main()
