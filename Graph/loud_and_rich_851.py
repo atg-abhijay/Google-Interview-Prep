8d867d4baa59d38e737616822d06647ad5867d89
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-11 16:08:31 -0500

    Partially solve 851. Loud and Rich: Time Limit Exceeded
    - Build a directed graph using the input list 'richer' where an
    edge is directed towards the richer person for each pair in 'richer'.
    
    - For each person, run a DFS that traverses through it's progressively
    richer neighbours and keep track of the lowest quietness along the way.
    
    - For a person p, once the DFS is done, you will get a person q who
    is the least quiet amongst all the people who definitely have equal
    to or more money than p (q can be equal to p as well).
    
    - Possible problem right now: There are repeated calculations
    and additions to the stack happening. These can probably be
    reduced.
    
    71 / 86 test cases passed.

--- Graph/loud_and_rich_851.py
+++ Graph/loud_and_rich_851.py
@@ -11,7 +11,41 @@ class Solution(object):
         :type quiet: List[int]
         :rtype: List[int]
         """
-        return []
+        # If there is no information about
+        # anybody being richer than anybody else
+        if not richer:
+            return list(range(len(quiet)))
+
+        # Build an adjacency list representation
+        # of the graph arising from the input.
+        # Make a directed graph where the edge
+        # is directed towards the richer person
+        # for each pair of people in the input
+        num_people = len(quiet)
+        graph = [[] for _ in range(num_people)]
+        for person_a, person_b in richer:
+            graph[person_b].append(person_a)
+
+        answer = [-1] * num_people
+        for person in range(num_people):
+            # Answer for this person has
+            # already been determined
+            # if answer[person] != -1:
+            #     continue
+
+            least_quiet_richer = person
+            stack = [person]
+            while stack:
+                p = stack.pop()
+                if quiet[p] < quiet[least_quiet_richer]:
+                    least_quiet_richer = p
+
+                for richer_person in graph[p]:
+                    stack.append(richer_person)
+
+            answer[person] = least_quiet_richer
+
+        return answer
 
 
 def main():
