fc411440390c01a1acb2e3417f03777e88d63b2f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-11 09:53:01 -0500

    Attempt 1 at 1319. Number of Operations to Make Network Connected
    - Construct a (directed) graph from the connections. Don't need
    the graph to be undirected.
    
    - Run (multiple instances of) BFS on the possibly disconnected
    graph of connections (hence the use of a set containing all the
    computers). By running a BFS, we would create a tree that's using
    the minimum number of edges to connect everything that's connected
    to each other.
    
    - The edges that are therefore left over, can be used to make the
    entire graph connected. Each run of the BFS will generate a
    connected component. We can rethink a component to be a single
    node and we would therefore like to connect a bunch of isolated
    notes. We can create a path between them. To form a path using
    n nodes, we would need (n-1) edges.
    
    - Check if #edges that are left over after the BFSes are
    sufficient to create a path between the components/nodes.
    
    6 / 36 test cases passed.

--- Graph/num_operations_make_network_connected_1319.py
+++ Graph/num_operations_make_network_connected_1319.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/number-of-operations-to-make-network-connected/
 """
 
 
+from collections import defaultdict, deque
+
+
 class Solution(object):
     def makeConnected(self, n, connections):
         """
@@ -11,6 +14,31 @@ class Solution(object):
         :type connections: List[List[int]]
         :rtype: int
         """
+        # Build an adjacency list representation
+        # of the graph of connections in the network
+        graph = defaultdict(list)
+        for node_a, node_b in connections:
+            graph[node_a].append(node_b)
+
+        num_conns = len(connections)
+        computers, num_bfs_edges = set(range(n)), 0
+        visited, num_discontd_networks = set(), 0
+        while computers:
+            queue = deque([computers.pop()])
+            visited.add(queue[0])
+            num_discontd_networks += 1
+            while queue:
+                computer = queue.popleft()
+                computers.discard(computer)
+                for nbr in graph[computer]:
+                    if nbr not in visited:
+                        visited.add(nbr)
+                        queue.append(nbr)
+                        num_bfs_edges += 1
+
+        if num_conns - num_bfs_edges >= num_discontd_networks - 1:
+            return num_discontd_networks - 1
+
         return -1
 
 
