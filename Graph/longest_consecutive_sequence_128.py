da5053ab48d6ede41d9c836dbb9adc012ab3436e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-11 19:59:21 -0500

    128 - 'Solve' the problem: solution is accepted but is quite slow

--- Graph/longest_consecutive_sequence_128.py
+++ Graph/longest_consecutive_sequence_128.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/longest-consecutive-sequence/
 """
 
 
+from collections import defaultdict
+
+
 class Solution(object):
     def longestConsecutive(self, nums):
         """
@@ -13,13 +16,36 @@ class Solution(object):
         if not nums:
             return 0
 
-        min_val = min(nums)
+        min_val, max_val = min(nums), max(nums)
         num_elems = len(nums)
-        diffs = {x for x in range(num_elems)}
+        sequences = defaultdict(lambda: [0] * num_elems)
         for num in nums:
-            diffs.discard(num - min_val)
+            quotient, remainder = num // num_elems, num % num_elems
+            sequences[quotient][remainder] = 1
 
-        return num_elems - len(diffs)
+        longest_seq_len = 0
+        min_quotient, max_quotient = min_val // num_elems, max_val // num_elems
+        for quotient in range(min_quotient, max_quotient+1):
+            if quotient not in sequences:
+                continue
+
+            longest_seq_len = max(longest_seq_len, self.get_cnsctv_ones(sequences[quotient]))
+            if quotient + 1 in sequences:
+                longest_seq_len = max(longest_seq_len, self.get_cnsctv_ones(sequences[quotient] + sequences[quotient + 1]))
+
+        return longest_seq_len
+
+
+    def get_cnsctv_ones(self, sequence):
+        num_consecutive = max_consecutive = 0
+        for num in sequence:
+            if num == 1:
+                num_consecutive += 1
+            else:
+                max_consecutive = max(max_consecutive, num_consecutive)
+                num_consecutive = 0
+
+        return max(max_consecutive, num_consecutive)
 
 
 def main():
