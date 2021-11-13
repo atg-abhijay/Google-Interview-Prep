7ec8de06755a61ed9370b2817a0be08ae7699820
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-13 17:19:57 -0500

    2044. Count Number of Maximum Bitwise-OR Subsets: Initial commit

---
+++ Recursion/count_max_bitwise_subsets_2044.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
+"""
+
+
+class Solution(object):
+    def countMaxOrSubsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().countMaxOrSubsets([2, 2, 2]))
+
+
+if __name__ == "__main__":
+    main()
