47b3eabcc49e41053fa0ab90bcc4b30638e22c4f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-13 17:32:01 -0500

    Solve 2044. Count Number of Maximum Bitwise-OR Subsets
    - Use the binary-based approach from 78. Subsets to generate the
    subsets; and keep track of the maximum Bitwise-OR value.
    
    - Solution seems to be slow for some reason though: ~4500 ms, ~5%

--- Recursion/count_max_bitwise_subsets_2044.py
+++ Recursion/count_max_bitwise_subsets_2044.py
@@ -4,14 +4,28 @@ https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
 """
 
 
+from functools import reduce
+
+
 class Solution(object):
     def countMaxOrSubsets(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        return -1
+        num_elems = len(nums)
+        max_bitwise, num_subsets = 0, 0
+        for combn in range(2 ** num_elems, 2 ** (num_elems + 1)):
+            bitmask = bin(combn)[3:]
+            subset = [num for bit, num in zip(bitmask, nums) if bit == '1']
+            subset_OR = reduce(lambda x, y: x | y, subset, 0)
+            if subset_OR > max_bitwise:
+                max_bitwise = subset_OR
+                num_subsets = 1
+            elif subset_OR == max_bitwise:
+                num_subsets += 1
 
+        return num_subsets
 
 def main():
     print(Solution().countMaxOrSubsets([2, 2, 2]))
