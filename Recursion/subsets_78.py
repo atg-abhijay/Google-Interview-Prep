2fbb78765f490f99351c371d41accca983ef2a0d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-13 14:03:00 -0500

    78. Subsets: Initial commit*

--- Recursion/subsets_78.py
+++ Recursion/subsets_78.py
@@ -57,6 +57,14 @@ class Solution(object):
         return subsets
 
 
+    def subsets_2ndPass(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
 def main():
     print(Solution().subsets([1, 2, 3]))
 
