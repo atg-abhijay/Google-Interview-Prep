c76be59b3bfee73173b7d39a0d0204da684d5961
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-13 15:57:16 -0500

    90. Subsets II: Initial commit

---
+++ Recursion/subsets_II_90.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/subsets-ii/
+"""
+
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().subsetsWithDup([1, 2, 2]))
+
+
+if __name__ == "__main__":
+    main()
