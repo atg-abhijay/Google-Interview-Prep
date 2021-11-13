fb48f619e5dad2fdb6dbeb96e424300f52b216eb
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-13 16:41:26 -0500

    Solve 90. Subsets II
    - For the numbers that are repeated, the kinds of subsets that they
    yield are dependent on their quantities. For example, if 3 appears
    four times, the subsets for 3 are: [3], [3, 3], [3, 3, 3] and
    [3, 3, 3, 3].
    
    - Figure out the subsets for the repeated numbers separately. Then,
    for each of the subsets generated, extend the power set that exists
    so far.
    
    - Also, maintain a separate set 'visited' which will store the
    repeated numbers from the list that we have encountered before.
    These numbers have to be used only once so check their visit status.
    
    - For the numbers that are not repeated, build the power set in the
    same way that it was built for 78. Subsets.

--- Recursion/subsets_II_90.py
+++ Recursion/subsets_II_90.py
@@ -4,17 +4,37 @@ https://leetcode.com/problems/subsets-ii/
 """
 
 
+from collections import defaultdict
+from itertools import product
+
+
 class Solution(object):
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        return [[]]
+        power_set = [[]]
+        unique_nums = defaultdict(int)
+        for num in nums:
+            unique_nums[num] += 1
+
+        repeated_nums = set(filter(lambda num: unique_nums[num] > 1, unique_nums))
+        visited = set()
+        while nums:
+            num = nums.pop()
+            if num not in repeated_nums:
+                power_set.extend([p_set + [num] for p_set in power_set])
+            elif num not in visited:
+                sets = [[num] * x for x in range(1, unique_nums[num]+1)]
+                power_set.extend([p + s for p, s in product(power_set, sets)])
+                visited.add(num)
+
+        return power_set
 
 
 def main():
-    print(Solution().subsetsWithDup([1, 2, 2]))
+    print(Solution().subsetsWithDup([10, 9, 9, 6, 6, 6]))
 
 
 if __name__ == "__main__":
