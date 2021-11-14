5aa9075f477509f605da3663c6c746b74f2a5a74
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-14 16:47:11 -0500

    70 - Reduce space complexity to O(1) by only tracking last 2 values*

--- Dynamic-Programming/climbing_stairs_70.py
+++ Dynamic-Programming/climbing_stairs_70.py
@@ -22,16 +22,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # Time: O(n), Space: O(n)
+        # Time: O(n), Space: O(1)
         # Tags: Dynamic Programming
         if n == 1:
             return 1
 
-        ways = [1, 1]
+        ways_two_before, ways_one_before = 1, 1
+        ways_current = 0
         for _ in range(2, n+1):
-            ways.append(ways[-1] + ways[-2])
+            ways_current = ways_two_before + ways_one_before
+            ways_two_before = ways_one_before
+            ways_one_before = ways_current
 
-        return ways[n]
+        return ways_current
 
 
 def main():
