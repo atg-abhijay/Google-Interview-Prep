dcf3cb3fc245fe0536f09568fda5903021e8a478
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-14 18:05:45 -0500

    300 - Attempt a binary-based solution which generates all sequences
    - Time limit is exceeded because of generating 2^n sequences.
    
    22 / 54 test cases passed.

--- Dynamic-Programming/longest_increasing_subsequence_300.py
+++ Dynamic-Programming/longest_increasing_subsequence_300.py
@@ -10,7 +10,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return -1
+        max_length = 0
+        num_elems = len(nums)
+        for combn in range(2 ** num_elems, 2 ** (num_elems + 1)):
+            bitmask = bin(combn)[3:]
+            sequence = [num for switch, num in zip(bitmask, nums) if switch == '1']
+            if all(map(lambda xy: xy[0] < xy[1], zip(sequence, sequence[1:]))):
+                max_length = max(max_length, len(sequence))
+
+        return max_length
 
 
 def main():
