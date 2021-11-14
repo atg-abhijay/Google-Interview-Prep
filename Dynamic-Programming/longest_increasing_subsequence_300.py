43661980f01b9af569dc46640abfb82fcee090af
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-14 17:25:10 -0500

    300. Longest Increasing Subsequence: Initial commit

---
+++ Dynamic-Programming/longest_increasing_subsequence_300.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/longest-increasing-subsequence/
+"""
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+
+
+if __name__ == "__main__":
+    main()
