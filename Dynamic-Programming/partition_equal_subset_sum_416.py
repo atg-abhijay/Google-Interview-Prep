8ec0e33df735a8946fbfe237c074ef9fa3d257b4
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-18 21:44:19 -0500

    416. Partition Equal Subset Sum: Initial commit

---
+++ Dynamic-Programming/partition_equal_subset_sum_416.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/partition-equal-subset-sum/
+"""
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().canPartition([1, 2, 3, 5]))
+
+
+if __name__ == "__main__":
+    main()
