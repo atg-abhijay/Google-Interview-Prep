cbc2eb913b46c6b0e26ab1ada551d0de282cd706
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-18 23:10:06 -0500

    416 - Attempt an arithmetic-based solution
    - If the total sum is odd, then it is not possible to partition
    into 2 subsets with equal sum. The total sum has to be even for the
    list to be partitioned.
    
    - This translates to the question that we are looking for a selection
    of numbers from the list that have a sum of (total sum // 2). The
    leftover numbers will automatically have a sum of (total sum // 2)
    as well.
    
    - Sort the input numbers. Do a binary search on the list and keep
    track of the sum of the list upto the calculated index. If the sum
    upto that index is less than total sum // 2, continue with the
    binary search on the righthand-side of the list.
    
    - If the sum exceeds the target value, that means that the target
    value cannot be made from a selection of the numbers.
    
    64 / 116 test cases passed.
    
    The problem with this is that this approach is selecting a
    _contiguous_ selection of numbers for the sum. That may not lead
    to the answer always.
    
    For example, for [1, 1, 2, 2] (sorted), picking contiguous numbers
    leads to an answer of False when in fact it is possible to pick 1
    and 2 (non-contiguous).

--- Dynamic-Programming/partition_equal_subset_sum_416.py
+++ Dynamic-Programming/partition_equal_subset_sum_416.py
@@ -4,24 +4,31 @@ https://leetcode.com/problems/partition-equal-subset-sum/
 """
 
 
+from math import ceil
+
+
 class Solution(object):
     def canPartition(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        num_elems = len(nums)
-        for combn in range(2 ** num_elems, 2 ** (num_elems + 1)):
-            combn = bin(combn)[3:]
-            sum_a, sum_b = 0, 0
-            for switch, num in zip(combn, nums):
-                if switch == '1':
-                    sum_a += num
-                else:
-                    sum_b += num
+        total_sum = sum(nums)
+        if total_sum % 2 == 1 or len(nums) == 1:
+            return False
 
-            if sum_a == sum_b:
+        nums.sort()
+        target, curr_sum = total_sum // 2, 0
+        start_idx, stop_idx = 0, len(nums) - 1
+        while start_idx < stop_idx:
+            mid_idx = ceil((start_idx + stop_idx)/2)
+            curr_sum += sum(nums[start_idx:mid_idx])
+            if curr_sum < target:
+                start_idx = mid_idx
+            elif curr_sum == target:
                 return True
+            else:
+                return False
 
         return False
 
