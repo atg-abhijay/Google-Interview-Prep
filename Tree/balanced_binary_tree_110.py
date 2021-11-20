8b6500f81b169bc7a1fbf50484dac9476421df4e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-20 18:16:27 -0500

    110. Balanced Binary Tree: Initial commit

---
+++ Tree/balanced_binary_tree_110.py
@@ -0,0 +1,34 @@
+"""
+URL of problem:
+https://leetcode.com/problems/balanced-binary-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(Solution().isBalanced(root))
+
+
+if __name__ == "__main__":
+    main()
