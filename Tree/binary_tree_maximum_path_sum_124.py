584abc53f5c45c886b7cf8a965da7b5f936ae74e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-21 00:09:08 -0500

    124 - Change whitespaces*

--- Tree/binary_tree_maximum_path_sum_124.py
+++ Tree/binary_tree_maximum_path_sum_124.py
@@ -75,6 +75,7 @@ class Solution(object):
         """
         return self.determineMaxSumAtNode(root)[1]
 
+
     def determineMaxSumAtNode(self, node):
         if not node:
             return 0, float('-inf')
@@ -83,7 +84,6 @@ class Solution(object):
         right_sum, max_right = self.determineMaxSumAtNode(node.right)
 
         node_sum = max(node.val + left_sum, node.val + right_sum, node.val)
-
         max_overall = max(max_left, max_right, node_sum, node.val + left_sum + right_sum)
         return node_sum, max_overall
 
