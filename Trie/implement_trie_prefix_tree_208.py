4839c3913ae415578292afa0e63242c58c48c43f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 14:41:47 -0500

    208. Implement Trie (Prefix Tree): Initial commit

---
+++ Trie/implement_trie_prefix_tree_208.py
@@ -0,0 +1,30 @@
+"""
+URL of problem:
+https://leetcode.com/problems/implement-trie-prefix-tree/
+"""
+
+
+class Trie:
+    def __init__(self):
+        pass
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        return
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        return False
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        return False
