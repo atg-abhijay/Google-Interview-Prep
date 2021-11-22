a321a157757ac7f04688b1fcf54e0d5f0007bd33
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-21 23:11:10 -0500

    105 - Attempt a solution that uses the preorder as a list of 'roots'
    - Since a preorder traversal starts with the node and then moves onto
    the children, the numbers in preorder denote the roots of various
    subtrees. For these roots, check their indices in the inorder.
    
    - The numbers to the left of the index obtained will denote the left
    subtree for that number, and the numbers to the right of that index
    will make up the right subtree. Use the roots from preorder to build
    subtrees.
    
    - This works out pretty nicely for a (proper) test case I created.
    The only issue was when the preorder and inorder were the same,
    which denotes a path-like tree that grows to the right. But I
    handled that separately at the start of the function. Despite all
    of this, only a few test cases are accepted as of right now.
    
    9 / 203 test cases passed.
    
    - I believe the problem is in the way the indices for preorder
    are being selected for the recursive calls. Specifically, the issue
    that I'm observing right now is with the index of preorder for
    root.right. It seems like the index should not be 2.

--- Tree/construct_binary_tree_from_preorder_inorder_105.py
+++ Tree/construct_binary_tree_from_preorder_inorder_105.py
@@ -19,7 +19,26 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
         """
-        return None
+        if preorder == inorder:
+            root = None
+            for num in reversed(inorder):
+                root = TreeNode(num, right=root)
+
+            return root
+
+        if not inorder:
+            return None
+
+        if len(inorder) == 1:
+            return TreeNode(inorder[0])
+
+        root = TreeNode(preorder[0])
+        root_idx = inorder.index(root.val)
+
+        root.left = self.buildTree(preorder[1:], inorder[:root_idx])
+        root.right = self.buildTree(preorder[2:], inorder[root_idx + 1:])
+
+        return root
 
 
 def main():
