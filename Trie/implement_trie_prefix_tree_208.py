45f2a58c83b9cb36ef54a8dd62e2d2d48947e961
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 15:10:33 -0500

    Solve 208. Implement Trie (Prefix Tree)
    - The solution is a bit slow though: 350 ms, 8%

--- Trie/implement_trie_prefix_tree_208.py
+++ Trie/implement_trie_prefix_tree_208.py
@@ -6,25 +6,53 @@ https://leetcode.com/problems/implement-trie-prefix-tree/
 
 class Trie:
     def __init__(self):
-        pass
+        self.value = False
+        self.children = [None for _ in range(26)]
 
     def insert(self, word):
         """
         :type word: str
         :rtype: None
         """
-        return
+        alphabet = {letter: idx for idx, letter in enumerate(string.ascii_lowercase)}
+        current_node = self
+        for char in word:
+            letter_idx = alphabet[char]
+            if not current_node.children[letter_idx]:
+                current_node.children[letter_idx] = Trie()
+
+            current_node = current_node.children[letter_idx]
+
+        current_node.value = True
 
     def search(self, word):
         """
         :type word: str
         :rtype: bool
         """
-        return False
+        alphabet = {letter: idx for idx, letter in enumerate(string.ascii_lowercase)}
+        current_node = self
+        for char in word:
+            letter_idx = alphabet[char]
+            if not current_node.children[letter_idx]:
+                return False
+
+            current_node = current_node.children[letter_idx]
+
+        return current_node.value
 
     def startsWith(self, prefix):
         """
         :type prefix: str
         :rtype: bool
         """
-        return False
+        alphabet = {letter: idx for idx, letter in enumerate(string.ascii_lowercase)}
+        current_node = self
+        for char in prefix:
+            letter_idx = alphabet[char]
+            if not current_node.children[letter_idx]:
+                return False
+
+            current_node = current_node.children[letter_idx]
+
+        return True
