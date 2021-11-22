ef92dd99c8076906b0f7c6215d55633f81d006f0
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 16:51:01 -0500

    211 - Partially solve the problem
    - The trie here starts with a root node that doesn't hold any letter
    value of it's own. The letters start appearing from the root's children.
    
    - For the alphabet characters, traverse down the trie, looking for
    the next character from word. If at any point any of the characters
    is not found, return False.
    
    - For the '.' character, any of the children of the current node
    would be valid. Therefore, start a recursive search with those children
    as roots and the portion of the word that comes after that '.' If the
    remaining word is found using any of those children, return True, else
    return False.
    
    10 / 13 test cases passed.
    
    - The solution makes sense but something seems to be going wrong
    somewhere..

--- Trie/design_add_search_words_data_structure_211.py
+++ Trie/design_add_search_words_data_structure_211.py
@@ -4,15 +4,24 @@ https://leetcode.com/problems/design-add-and-search-words-data-structure/
 """
 
 
+from collections import defaultdict
+
+
 class WordDictionary:
     def __init__(self):
-        pass
+        self.value = False
+        self.children = defaultdict(WordDictionary)
 
     def addWord(self, word):
         """
         :type word: str
         :rtype: None
         """
+        current_node = self
+        for char in word:
+            current_node = current_node.children[char]
+            current_node.value = True
+
         return
 
     def search(self, word):
@@ -20,4 +29,33 @@ class WordDictionary:
         :type word: str
         :rtype: bool
         """
-        return False
+        current_node = self
+        for idx, char in enumerate(word):
+            if char == '.':
+                for child in current_node.children.values():
+                    if child.search(word[idx+1:]):
+                        return True
+
+                return False
+
+            if char not in current_node.children:
+                return False
+
+            current_node = current_node.children[char]
+
+        return True
+
+
+def main():
+    word_dict = WordDictionary()
+    word_dict.addWord("bad")
+    word_dict.addWord("dad")
+    word_dict.addWord("mad")
+    print(f"pad -> {word_dict.search('pad')}")
+    print(f"bad -> {word_dict.search('bad')}")
+    print(f".ad -> {word_dict.search('.ad')}")
+    print(f"b.. -> {word_dict.search('b..')}")
+
+
+if __name__ == "__main__":
+    main()
