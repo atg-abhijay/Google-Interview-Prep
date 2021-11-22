15e147981f96d5fef92caaeee406838bd42186d3
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-21 19:05:01 -0500

    235 - Refactor code*

--- Tree/lowest_common_ancestor_in_bst_235.py
+++ Tree/lowest_common_ancestor_in_bst_235.py
@@ -74,8 +74,7 @@ class Solution(object):
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
 
-        elif p.val > root.val and q.val > root.val:
+        if p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
 
-        else:
-            return root
+        return root
