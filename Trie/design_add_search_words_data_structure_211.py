213a59b2cf5d3aed659ddc5715cc71b8ef9f1766
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 15:50:35 -0500

    211. Design Add and Search Words Data Structure: Initial commit

---
+++ Trie/design_add_search_words_data_structure_211.py
@@ -0,0 +1,23 @@
+"""
+URL of problem:
+https://leetcode.com/problems/design-add-and-search-words-data-structure/
+"""
+
+
+class WordDictionary:
+    def __init__(self):
+        pass
+
+    def addWord(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        return
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        return False
