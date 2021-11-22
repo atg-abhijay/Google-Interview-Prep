3c447108cb4323f3efebb0c0bbaad893be93710d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 17:28:06 -0500

    212 - Try out the solution from 79. Word Search as an experiment
    - 79. Word Search is a question that was under the category Matrix.
    I had solved it by conducting DFSes from each of the cells to see
    if the word in question existed in the grid or not.
    
    - I get Time Limit Exceeded with the solution from 79. I think the
    desire here is that we construct a Trie and _then_ look up the words.
    
    42 / 62 test cases passed.

--- Trie/word_search_II_212.py
+++ Trie/word_search_II_212.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/word-search-ii/
 """
 
 
+from itertools import product
+
+
 class Solution(object):
     def findWords(self, board, words):
         """
@@ -11,7 +14,55 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        return []
+        return [word for word in words if self.exist(board, word)]
+
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        num_rows, num_cols = len(board), len(board[0])
+        range_rows, range_cols = range(num_rows), range(num_cols)
+        if len(word) > num_rows * num_cols:
+            return False
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        visited = [[0 for _ in range_cols] for _ in range_rows]
+        for row_idx, col_idx in product(range_rows, range_cols):
+            if board[row_idx][col_idx] != word[0]:
+                continue
+
+            visited = [[0 for _ in range_cols] for _ in range_rows]
+            indices = [row_idx, col_idx, 0]
+            arrays = [board, visited, directions]
+            if self.performDFS(indices, word, arrays):
+                return True
+
+        return False
+
+
+    def performDFS(self, indices, word, arrays):
+        row_idx, col_idx, word_idx = indices
+        board, visited, directions = arrays
+        if board[row_idx][col_idx] != word[word_idx]:
+            return False
+
+        if word_idx == len(word) - 1:
+            return True
+
+        visited[row_idx][col_idx] = 1
+        is_found = False
+        for incr_x, incr_y in directions:
+            nbr_row, nbr_col = row_idx + incr_x, col_idx + incr_y
+            if 0 <= nbr_row < len(board) and 0 <= nbr_col < len(board[0]):
+                if visited[nbr_row][nbr_col]:
+                    continue
+
+                is_found |= self.performDFS([nbr_row, nbr_col, word_idx + 1], word, arrays)
+                visited[nbr_row][nbr_col] = 0
+
+        return is_found
 
 
 def main():
