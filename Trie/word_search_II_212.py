0cdc0c43419a970277bd2f8021e9212c753c6d53
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-22 23:57:30 -0500

    212 - Solution is accepted but is quite slow: 9200 ms, 10%
    - Remove the search and startsWith methods for Trie since they are
    not being used.
    
    - So, I was reading on the discussion board that people are getting
    a lot of Time Limit Exceeded even though their answers seem to be
    correct. So, people suggested some modifications here and there.
    Change the type for children to a regular dict instead of defaultdict.
    Not sure if it's offering any real improvement in performance..
    
    - In performDFS, if a node with a True value is encountered, append
    the path to the list and also set the node's value to False so that
    the same path isn't appended once more. I was simply using a set
    before to deal with this, without having to set the node's value
    to False but people in the discussion board did this. With this
    modification, we don't need to use a set since a path will only
    get added once.
    
    - Change the way the visit status is being handled. Basically, we
    want to avoid a node from being visited again if it's already part
    of the DFS stack. People seemed to have done this by changing the
    cell's value to a non-alphabet value. This way, the cell cannot
    belong anywhere in the Trie and it will be skipped over. And, once
    we're done DFSing into that node, we restore the value for that cell.
    This achieves the same functionality of maintaining a visit status.
    
    - Not sure if there's actually something wrong with the solution or
    not. These 'modifications' aren't anything big and don't seem like
    the 'proper' way to deal with this Time Limit Exceeded issue.

--- Trie/word_search_II_212.py
+++ Trie/word_search_II_212.py
@@ -5,44 +5,24 @@ https://leetcode.com/problems/word-search-ii/
 
 
 from itertools import product
-from collections import defaultdict
 
 
 class Trie:
     def __init__(self):
         self.value = False
-        self.children = defaultdict(Trie)
+        self.children = {}
 
 
     def insert(self, word):
         current_node = self
         for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = Trie()
+
             current_node = current_node.children[char]
 
         current_node.value = True
         return
-
-
-    def search(self, word):
-        current_node = self
-        for char in word:
-            if char not in current_node.children:
-                return False
-
-            current_node = current_node.children[char]
-
-        return current_node.value
-
-
-    def startsWith(self, prefix):
-        current_node = self
-        for char in prefix:
-            if char not in current_node.children:
-                return False
-
-            current_node = current_node.children[char]
-
-        return True
 
 
 class Solution:
@@ -54,7 +34,7 @@ class Solution:
         # West, North, East, South
         self.directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
         self.words_trie = Trie()
-        self.result = set()
+        self.result = []
 
 
     def findWords(self, board, words):
@@ -66,27 +46,28 @@ class Solution:
         for row_idx, col_idx in product(range(self.num_rows), range(self.num_cols)):
             char = self.board[row_idx][col_idx]
             if char in self.words_trie.children:
-                self.visited.add((row_idx, col_idx))
+                self.board[row_idx][col_idx] = "X"
                 self.performDFS(row_idx, col_idx, [char], self.words_trie.children[char])
-                self.visited.clear()
+                self.board[row_idx][col_idx] = char
 
         return self.result
 
 
     def performDFS(self, row_idx, col_idx, path, curr_node):
         if curr_node.value:
-            self.result.add(''.join(path))
+            self.result.append(''.join(path))
+            curr_node.value = False
 
         for row_diff, col_diff in self.directions:
             nbr_row, nbr_col = row_idx + row_diff, col_idx + col_diff
             within_bounds = 0 <= nbr_row < self.num_rows and 0 <= nbr_col < self.num_cols
-            if within_bounds and (nbr_row, nbr_col) not in self.visited:
+            if within_bounds:
                 nbr_char = self.board[nbr_row][nbr_col]
                 if nbr_char in curr_node.children:
                     path.append(nbr_char)
-                    self.visited.add((nbr_row, nbr_col))
+                    self.board[nbr_row][nbr_col] = "X"
                     self.performDFS(nbr_row, nbr_col, path, curr_node.children[nbr_char])
-                    self.visited.remove((nbr_row, nbr_col))
+                    self.board[nbr_row][nbr_col] = nbr_char
                     path.pop()
 
         return
@@ -95,13 +76,8 @@ class Solution:
 def main():
     print(
         Solution().findWords(
-            [
-                ["o", "a", "a", "n"],
-                ["e", "t", "a", "e"],
-                ["i", "h", "k", "r"],
-                ["i", "f", "l", "v"],
-            ],
-            ["oath", "pea", "eat", "rain"],
+            [["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"]],
+            ["lllllll","fffffff","ssss","s","rr","xxxx","ttt","eee","ppppppp","iiiiiiiii","xxxxxxxxxx","pppppp","xxxxxx","yy","jj","ccc","zzz","ffffffff","r","mmmmmmmmm","tttttttt","mm","ttttt","qqqqqqqqqq","z","aaaaaaaa","nnnnnnnnn","v","g","ddddddd","eeeeeeeee","aaaaaaa","ee","n","kkkkkkkkk","ff","qq","vvvvv","kkkk","e","nnn","ooo","kkkkk","o","ooooooo","jjj","lll","ssssssss","mmmm","qqqqq","gggggg","rrrrrrrrrr","iiii","bbbbbbbbb","aaaaaa","hhhh","qqq","zzzzzzzzz","xxxxxxxxx","ww","iiiiiii","pp","vvvvvvvvvv","eeeee","nnnnnnn","nnnnnn","nn","nnnnnnnn","wwwwwwww","vvvvvvvv","fffffffff","aaa","p","ddd","ppppppppp","fffff","aaaaaaaaa","oooooooo","jjjj","xxx","zz","hhhhh","uuuuu","f","ddddddddd","zzzzzz","cccccc","kkkkkk","bbbbbbbb","hhhhhhhhhh","uuuuuuu","cccccccccc","jjjjj","gg","ppp","ccccccccc","rrrrrr","c","cccccccc","yyyyy","uuuu","jjjjjjjj","bb","hhh","l","u","yyyyyy","vvv","mmm","ffffff","eeeeeee","qqqqqqq","zzzzzzzzzz","ggg","zzzzzzz","dddddddddd","jjjjjjj","bbbbb","ttttttt","dddddddd","wwwwwww","vvvvvv","iii","ttttttttt","ggggggg","xx","oooooo","cc","rrrr","qqqq","sssssss","oooo","lllllllll","ii","tttttttttt","uuuuuu","kkkkkkkk","wwwwwwwwww","pppppppppp","uuuuuuuu","yyyyyyy","cccc","ggggg","ddddd","llllllllll","tttt","pppppppp","rrrrrrr","nnnn","x","yyy","iiiiiiiiii","iiiiii","llll","nnnnnnnnnn","aaaaaaaaaa","eeeeeeeeee","m","uuu","rrrrrrrr","h","b","vvvvvvv","ll","vv","mmmmmmm","zzzzz","uu","ccccccc","xxxxxxx","ss","eeeeeeee","llllllll","eeee","y","ppppp","qqqqqq","mmmmmm","gggg","yyyyyyyyy","jjjjjj","rrrrr","a","bbbb","ssssss","sss","ooooo","ffffffffff","kkk","xxxxxxxx","wwwwwwwww","w","iiiiiiii","ffff","dddddd","bbbbbb","uuuuuuuuu","kkkkkkk","gggggggggg","qqqqqqqq","vvvvvvvvv","bbbbbbbbbb","nnnnn","tt","wwww","iiiii","hhhhhhh","zzzzzzzz","ssssssssss","j","fff","bbbbbbb","aaaa","mmmmmmmmmm","jjjjjjjjjj","sssss","yyyyyyyy","hh","q","rrrrrrrrr","mmmmmmmm","wwwww","www","rrr","lllll","uuuuuuuuuu","oo","jjjjjjjjj","dddd","pppp","hhhhhhhhh","kk","gggggggg","xxxxx","vvvv","d","qqqqqqqqq","dd","ggggggggg","t","yyyy","bbb","yyyyyyyyyy","tttttt","ccccc","aa","eeeeee","llllll","kkkkkkkkkk","sssssssss","i","hhhhhh","oooooooooo","wwwwww","ooooooooo","zzzz","k","hhhhhhhh","aaaaa","mmmmm"]
         )
     )
 
