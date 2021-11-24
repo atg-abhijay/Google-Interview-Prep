f4ef8b5e90d4951035caf83abfacc4296a9d3b76
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-24 01:49:48 -0500

    1976 - Find #paths with shortest time to get to the destination
    - Use Dijkstra's algorithm to determine the shortest path from the
    source to the destination. Then, initiate a DFS from the source
    and enumerate all the paths. At any moment, if the time for a path
    exceeds the shortest time, do not recurse further down that path.
    
    - Find all the paths for which the path time is equal to the
    shortest path time. The 'path' variable isn't really needed. I was
    simply using it to print which paths fit the bill. It can be removed.
    
    16 / 54 test cases passed.
    
    - Getting Time Limit Exceeded. Not sure what kind of optimization
    is needed yet.

--- Graph/num_ways_arrive_at_destination_1976.py
+++ Graph/num_ways_arrive_at_destination_1976.py
@@ -8,7 +8,14 @@ from collections import defaultdict
 import heapq
 
 
-class Solution(object):
+class Solution:
+    def __init__(self):
+        self.graph = None
+        self.visited = set()
+        self.shortest_time = 0
+        self.target = -1
+        self.num_ways = 0
+
     def countPaths(self, n, roads):
         """
         :type n: int
@@ -17,22 +24,46 @@ class Solution(object):
         """
         # Build an adjacency list graph
         # representation of the input
-        graph = defaultdict(dict)
+        self.graph = defaultdict(dict)
         for u_node, v_node, time in roads:
-            graph[u_node][v_node] = time
-            # graph[v_node][u_node] = time
+            self.graph[u_node][v_node] = time
+            self.graph[v_node][u_node] = time
 
-        min_times = {node: float('inf') for node in graph}
-        min_times['s'] = 0
-        heap, visited = [(0, 's')], set()
+        self.target = n - 1
+        self.determineShortestPath(n)
+        self.visited.add(0)
+        self.performDFS(0, 0, [0])
+        return self.num_ways
+
+    def performDFS(self, curr_node, path_time, path):
+        if curr_node == self.target:
+            self.num_ways += 1
+            print(path)
+            return
+
+        for nbr, travel_time in self.graph[curr_node].items():
+            new_time = path_time + travel_time
+            if nbr not in self.visited and new_time <= self.shortest_time:
+                self.visited.add(nbr)
+                path.append(nbr)
+                self.performDFS(nbr, new_time, path)
+                path.pop()
+                self.visited.remove(nbr)
+
+        return
+
+    def determineShortestPath(self, n):
+        # Use Dijkstra's algorithm
+        min_times = {node: float("inf") for node in range(n)}
+        min_times[0] = 0
+        heap, visited = [(0, 0)], set()
         while heap:
             _, curr_node = heapq.heappop(heap)
             if curr_node in visited:
                 continue
 
             visited.add(curr_node)
-
-            for nbr, travel_time in graph[curr_node].items():
+            for nbr, travel_time in self.graph[curr_node].items():
                 if nbr in visited:
                     continue
 
@@ -42,7 +73,7 @@ class Solution(object):
                     min_times[nbr] = new_cost
                     heapq.heappush(heap, (new_cost, nbr))
 
-        return min_times
+        self.shortest_time = min_times[n - 1]
 
 
 def main():
@@ -50,16 +81,16 @@ def main():
         Solution().countPaths(
             n=7,
             roads=[
-                ['s', 't', 10],
-                ['s', 'y', 5],
-                ['t', 'y', 2],
-                ['t', 'x', 2],
-                ['y', 't', 1],
-                ['y', 'x', 4],
-                ['y', 'z', 6],
-                ['x', 'z', 2],
-                ['z', 'x', 7],
-                ['z', 's', 3],
+                [0, 6, 7],
+                [0, 1, 2],
+                [1, 2, 3],
+                [1, 3, 3],
+                [6, 3, 3],
+                [3, 5, 1],
+                [6, 5, 1],
+                [2, 5, 1],
+                [0, 4, 5],
+                [4, 6, 2],
             ],
         )
     )
