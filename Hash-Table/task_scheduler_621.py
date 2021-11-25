22e0155c9fd9d54ef7811a770f8b960c766de544
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-25 15:41:20 -0500

    621 - Check for non-emptiness of leftover heap too
    - We keep iterating while either of the heaps has tasks in it.
    
    - Refactor the code a bit to simplify things. We use the CPU heap
    if the CPU heap is not empty and if the overall time is at least
    as much as the top task in the CPU heap. Otherwise, we look towards
    using the tasks from the leftover heap. If the leftover heap is
    empty however, the CPU then must remain idle.
    
    - Also, once a task is worked upon, we put it back in the CPU heap
    if it's count hasn't become 0. And we update the time for it to
    be n + 1 steps away from the current overall time, since that's the
    amount of time we need to wait before being able to perform this
    task again.
    
    - Remove the code for the previous attempts.
    
    63 / 71 test cases passed.
    
    - There is a test case that's failing wherein it is more preferable
    to evenly use the tasks rather than focusing on using the higher
    priority tasks to the maximum extent. I think the idea with that is
    that if the tasks are evenly spread out, they provide a buffer to
    each other during the cooldown/waiting period.
    
    - But, this even distribution approach doesn't seem to work for
    other test cases.. Probably need to bring these two ideas together
    somehow.

--- Hash-Table/task_scheduler_621.py
+++ Hash-Table/task_scheduler_621.py
@@ -28,17 +28,15 @@ class Solution:
             heapq.heappush(tasks_heap, (0, -1 * count, task))
 
         global_time = 0
-        while cpu_heap:
-            allowed_time, _, _ = cpu_heap[0]
-            if global_time < allowed_time:
-                if tasks_heap:
-                    _, neg_count, top_task = heapq.heappop(tasks_heap)
-                else:
-                    global_time += 1
-                    # print("idle -> ", end=" ")
-                    continue
-            else:
+        while cpu_heap or tasks_heap:
+            if cpu_heap and global_time >= cpu_heap[0][0]:
                 _, neg_count, top_task = heapq.heappop(cpu_heap)
+            elif tasks_heap:
+                _, neg_count, top_task = heapq.heappop(tasks_heap)
+            else:
+                global_time += 1
+                # print("idle -> ", end=" ")
+                continue
 
             # print(f"{top_task} -> ", end=" ")
             updated_count = neg_count + 1
@@ -49,65 +47,6 @@ class Solution:
 
         # print()
         return global_time
-
-        task_details = {}
-        for task in tasks:
-            # Use a negative count to emulate a max heap
-            if task not in task_details:
-                task_details[task] = -1
-            else:
-                task_details[task] -= 1
-
-        # Push (remaining cooldown time, count, task)
-        # The cooldown time will be the first
-        # priority and then the count for the task
-        cpu_heap, current_time = [], 0
-        for task, count in task_details.items():
-            heapq.heappush(cpu_heap, (0, count, task))
-
-        # Alternate approach:
-        # Here, the cooldown time is not decremented.
-        # Rather, the current time is incremented and
-        # that is used as a basis for selecting the
-        # next task. Still giving problems though.
-        # while heap:
-        #     if heap[0][0] > current_time:
-        #         print("idle -> ", end=" ")
-        #     else:
-        #         _, count, top_task = heapq.heappop(heap)
-        #         print(f"{top_task} -> ", end=" ")
-        #         if count + 1 != 0:
-        #             heapq.heappush(heap, (n + current_time + 1, count + 1, top_task))
-
-        #     current_time += 1
-
-        # return current_time
-
-        while cpu_heap:
-            # If the top priority task doesn't have a remaining
-            # cooldown time of 0, then the CPU has to be idle
-            if cpu_heap[0][0] == 0:
-                _, count, top_task = heapq.heappop(cpu_heap)
-                print(f"{top_task} -> ", end=" ")
-                if count + 1 != 0:
-                    heapq.heappush(cpu_heap, (n + 1, count + 1, top_task))
-            else:
-                print("idle -> ", end=" ")
-
-            # Decrement the cooldown times and heapify the heap
-            are_cooldowns_updated = False
-            for idx, (cooldown_left, count, task) in enumerate(cpu_heap):
-                if cooldown_left != 0:
-                    cpu_heap[idx] = (max(0, cooldown_left - 1), count, task)
-                    are_cooldowns_updated = True
-
-            if are_cooldowns_updated:
-                heapq.heapify(cpu_heap)
-
-            current_time += 1
-
-        print()
-        return current_time
 
 
 def main():
