ef2430cdaf1229b5534c58818320500b9cca456c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-25 14:36:46 -0500

    621 - Use two heaps for the tasks
    - Use one heap for keeping track of the tasks that have been
    performed by the CPU at least once. In the other 'leftover' heap,
    place the tasks that haven't been performed by the CPU at all.
    
    - To start, put the task with the highest count in the CPU heap
    since we want to start working on that task at the earliest.
    
    - Use an overall timestamp to keep track of where we are in the
    process.
    
    - The general idea is that we want to start the tasks with the
    highest counts early and that we want to do them as often as
    possible too since they're the ones that are going to take the
    longest to finish.
    
    - While iterating over the CPU heap, check if the 'top' task
    has an allowed/permitted time that is less than the overall
    time. If it doesn't, that means that we need to wait a little
    bit more before we can do that task.
    
    - Look to the leftover heap then. Pick the highest count task
    from there and work on that. Since the leftover heap contains
    tasks that have never been worked on, there are no restrictions
    in terms of any waiting time.
    
    - If the CPU's top task is not permitted yet and the leftover
    heap doesn't contain anything, then the CPU must remain idle.
    
    - One large testcase that used to timeout before is now passing.
    
    57 / 71 test cases passed.
    
    - Something seems to be going wrong somewhere..

--- Hash-Table/task_scheduler_621.py
+++ Hash-Table/task_scheduler_621.py
@@ -8,7 +8,7 @@ from collections import Counter
 import heapq
 
 
-class Solution(object):
+class Solution:
     def leastInterval(self, tasks, n):
         """
         :type tasks: List[str]
@@ -19,30 +19,35 @@ class Solution(object):
             return len(tasks)
 
         tasks_counts = Counter(tasks)
-        # top_task, max_count = tasks_counts.most_common(1)[0]
-        heap = []
+        top_task, max_count = tasks_counts.most_common(1)[0]
+        cpu_heap, tasks_heap = [(0, -1 * max_count, top_task)], []
+        del tasks_counts[top_task]
         for task, count in tasks_counts.items():
             # Use a negative count to emulate a max heap
             # Tuple: (permitted time, negative count, task)
-            heapq.heappush(heap, (0, -1 * count, task))
+            heapq.heappush(tasks_heap, (0, -1 * count, task))
 
         global_time = 0
-        while heap:
-            allowed_time, _, _ = heap[0]
+        while cpu_heap:
+            allowed_time, _, _ = cpu_heap[0]
             if global_time < allowed_time:
-                global_time += 1
-                print("idle -> ", end=" ")
-                continue
+                if tasks_heap:
+                    _, neg_count, top_task = heapq.heappop(tasks_heap)
+                else:
+                    global_time += 1
+                    # print("idle -> ", end=" ")
+                    continue
+            else:
+                _, neg_count, top_task = heapq.heappop(cpu_heap)
 
-            allowed_time, neg_count, top_task = heapq.heappop(heap)
-            print(f"{top_task} -> ", end=" ")
+            # print(f"{top_task} -> ", end=" ")
             updated_count = neg_count + 1
             if updated_count != 0:
-                heapq.heappush(heap, (global_time + n + 1, updated_count, top_task))
+                heapq.heappush(cpu_heap, (global_time + n + 1, updated_count, top_task))
 
             global_time += 1
 
-        print()
+        # print()
         return global_time
 
         task_details = {}
@@ -56,9 +61,9 @@ class Solution(object):
         # Push (remaining cooldown time, count, task)
         # The cooldown time will be the first
         # priority and then the count for the task
-        heap, current_time = [], 0
+        cpu_heap, current_time = [], 0
         for task, count in task_details.items():
-            heapq.heappush(heap, (0, count, task))
+            heapq.heappush(cpu_heap, (0, count, task))
 
         # Alternate approach:
         # Here, the cooldown time is not decremented.
@@ -78,26 +83,26 @@ class Solution(object):
 
         # return current_time
 
-        while heap:
+        while cpu_heap:
             # If the top priority task doesn't have a remaining
             # cooldown time of 0, then the CPU has to be idle
-            if heap[0][0] == 0:
-                _, count, top_task = heapq.heappop(heap)
+            if cpu_heap[0][0] == 0:
+                _, count, top_task = heapq.heappop(cpu_heap)
                 print(f"{top_task} -> ", end=" ")
                 if count + 1 != 0:
-                    heapq.heappush(heap, (n + 1, count + 1, top_task))
+                    heapq.heappush(cpu_heap, (n + 1, count + 1, top_task))
             else:
                 print("idle -> ", end=" ")
 
             # Decrement the cooldown times and heapify the heap
             are_cooldowns_updated = False
-            for idx, (cooldown_left, count, task) in enumerate(heap):
+            for idx, (cooldown_left, count, task) in enumerate(cpu_heap):
                 if cooldown_left != 0:
-                    heap[idx] = (max(0, cooldown_left - 1), count, task)
+                    cpu_heap[idx] = (max(0, cooldown_left - 1), count, task)
                     are_cooldowns_updated = True
 
             if are_cooldowns_updated:
-                heapq.heapify(heap)
+                heapq.heapify(cpu_heap)
 
             current_time += 1
 
@@ -108,7 +113,7 @@ class Solution(object):
 def main():
     print(
         Solution().leastInterval(
-            ["A","A","A","B","B","B"], n = 2
+            ["A","A","A","B","B","B","C","D","E","F","G","H","I","J","K"], n = 7
         )
     )
 
