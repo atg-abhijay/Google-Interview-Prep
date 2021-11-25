3243eed6bd58b31c4e6aa0eab675626589c12990
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-25 00:42:27 -0500

    40. Combination Sum II: Initial commit

---
+++ Dynamic-Programming/combination_sum_II_40.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/combination-sum-ii/
+"""
+
+
+class Solution:
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
+
+
+if __name__ == "__main__":
+    main()
