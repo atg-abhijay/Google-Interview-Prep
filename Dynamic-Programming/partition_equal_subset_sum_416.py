289f42599dbee2c7736128a4ece0fcd2674ba324
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-24 22:48:09 -0500

    Solve 416. Partition Equal Subset Sum
    - Recurse in the same way as before but cache results in a
    defaultdict once a result is determined so that we can refer to
    it and don't have to recalculate it.
    
    - For the caching in the defaultdict, let the following be the
    structure: {idx : {target : True/False} }. idx refers to the fact
    that we are considering the input list starting from idx. target
    refers to an attempt to achieve a sum of 'target' using the input
    list starting from idx. The final value True/False denotes whether
    or not it is possible to achieve 'target' using the input list
    starting from idx.
    
    - Also, if a True is returned at any point in time, echo that back
    to the top since that is sufficient and we don't need to do any
    further computations.
    
    - Seems like a pretty nice idea. Do a regular recursive solution
    and then find repeated computations and cache them. I wonder if
    that's the way for all of these dynamic programming questions??

--- Dynamic-Programming/partition_equal_subset_sum_416.py
+++ Dynamic-Programming/partition_equal_subset_sum_416.py
@@ -4,10 +4,13 @@ https://leetcode.com/problems/partition-equal-subset-sum/
 """
 
 
-from itertools import product
+from collections import defaultdict
 
 
-class Solution(object):
+class Solution:
+    def __init__(self):
+        self.possible = defaultdict(lambda: defaultdict(bool))
+
     def canPartition(self, nums):
         """
         :type nums: List[int]
@@ -17,18 +20,29 @@ class Solution(object):
         if total_sum % 2 == 1 or len(nums) == 1:
             return False
 
-        return self.partition(nums, total_sum // 2)
+        return self.partition(0, total_sum // 2, nums)
 
-    def partition(self, nums, target):
-        if target < 0 or not nums:
+    def partition(self, idx, target, nums):
+        if target < 0 or not nums[idx:]:
+            self.possible[idx][target] = False
             return False
 
         if target == 0:
+            self.possible[idx][target] = True
             return True
 
-        exclude_result = self.partition(nums[1:], target)
-        include_result = self.partition(nums[1:], target - nums[0])
+        if target not in self.possible[idx + 1]:
+            if self.partition(idx + 1, target, nums):
+                return True
 
+        exclude_result = self.possible[idx + 1][target]
+
+        sub_target = target - nums[idx]
+        if sub_target not in self.possible[idx + 1]:
+            if self.partition(idx + 1, sub_target, nums):
+                return True
+
+        include_result = self.possible[idx + 1][sub_target]
         return include_result or exclude_result
 
 
