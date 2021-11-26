145560788b1ae8f95d22208c91f81c6aa4b272b2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 16:51:10 -0500

    49. Group Anagrams: Initial commit

---
+++ String/group_anagrams_49.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/group-anagrams/
+"""
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+
+if __name__ == "__main__":
+    main()
