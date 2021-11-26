c940bd800e80f96c5f1cb493385fb9499beaa6c1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 18:52:45 -0500

    647 - Attempt to solve the problem using binary search
    - Start from the entire string. Find the middle index. If the
    characters surrounding the middle are equal, update the palindromes
    and continue moving outwards until the characters are equal.
    
    - When we get to a point where the characters are not equal, split
    into a left and right binary search on either side of the middle
    to find palindromes from slices of the string.
    
    - Maintain a visited set so that we don't visit combinations of
    (start_index, stop_index) repeatedly.
    
    - This seems to be working for palindromes of odd length but is
    failing for those of even length.
    
    15 / 130 test cases passed.

--- String/palindromic_substrings_647.py
+++ String/palindromic_substrings_647.py
@@ -5,16 +5,51 @@ https://leetcode.com/problems/palindromic-substrings/
 
 
 class Solution:
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        return -1
+    def __init__(self):
+        self.num_palinds = 0
+        self.palindromes = []
+        self.str_len = 0
+        self.visited = set()
+
+    def countSubstrings(self, s: str) -> int:
+        if len(s) == 1:
+            return 1
+
+        self.str_len = len(s)
+        self.palindromes.extend([char for char in s])
+        self.num_palinds += self.str_len
+
+        self.checkIdx(s, 0, self.str_len - 1)
+        print(self.palindromes)
+        return self.num_palinds
+
+    def checkIdx(self, string, start_idx, stop_idx):
+        if start_idx == stop_idx:
+            return
+
+        mid_idx = (start_idx + stop_idx) // 2
+        left_idx, right_idx = mid_idx - 1, mid_idx + 1
+        while left_idx >= 0 and right_idx <= self.str_len - 1:
+            if string[left_idx] == string[right_idx]:
+                self.num_palinds += 1
+                self.palindromes.append(string[left_idx:right_idx+1])
+                left_idx -= 1
+                right_idx += 1
+            else:
+                left_idxs, right_idxs = (start_idx, mid_idx), (mid_idx + 1, stop_idx)
+                if left_idxs not in self.visited:
+                    self.visited.add(left_idxs)
+                    self.checkIdx(string, *left_idxs)
+                if right_idxs not in self.visited:
+                    self.visited.add(right_idxs)
+                    self.checkIdx(string, *right_idxs)
+                break
+
+        return
 
 
 def main():
-    print(Solution().countSubstrings("aaa"))
+    print(Solution().countSubstrings("racecarbob"))
 
 
 if __name__ == "__main__":
