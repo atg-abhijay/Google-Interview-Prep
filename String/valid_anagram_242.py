cf9de84110b42e773f03d40d50ae7b476611025f
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 16:44:33 -0500

    242. Valid Anagram: Initial commit

---
+++ String/valid_anagram_242.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/valid-anagram/
+"""
+
+
+class Solution:
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().isAnagram(s="anagram", t="nagaram"))
+
+
+if __name__ == "__main__":
+    main()
