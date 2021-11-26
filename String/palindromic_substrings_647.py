eef91d0306c415b42e6d762da37c8a1e46dda67c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 17:21:31 -0500

    647. Palindromic Substrings: Initial commit

---
+++ String/palindromic_substrings_647.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/palindromic-substrings/
+"""
+
+
+class Solution:
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().countSubstrings("aaa"))
+
+
+if __name__ == "__main__":
+    main()
