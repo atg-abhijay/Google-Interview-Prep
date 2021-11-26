84c71a91745940143a23d41e240e2c27c0837b10
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 16:29:43 -0500

    125. Valid Palindrome: Initial commit

---
+++ String/valid_palindrome_125.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/valid-palindrome/
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
+
+
+if __name__ == "__main__":
+    main()
