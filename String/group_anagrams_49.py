eea6670bea8c3e08fd4aa46d56f845e6924ffcd5
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 17:05:33 -0500

    Solve 49. Group Anagrams
    - Words will have the same anagrams if they have the same counts
    for the same letters. Create a dictionary where the key will be
    the Counter for a string and the value will be the list of strings
    that yield that Counter.
    
    - Since Counter objects are not hashable, obtain the (key, value)
    pairs of (letter, count), sort them so that same Counters with
    different orderings are all brought together, and then create a
    tuple out of them so that we get a key that's hashable for the
    dictionary.

--- String/group_anagrams_49.py
+++ String/group_anagrams_49.py
@@ -4,13 +4,20 @@ https://leetcode.com/problems/group-anagrams/
 """
 
 
+from collections import Counter, defaultdict
+
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        return [[]]
+        anagrams = defaultdict(list)
+        for string in strs:
+            anagrams[tuple(sorted(Counter(string).items()))].append(string)
+
+        return list(anagrams.values())
 
 
 def main():
