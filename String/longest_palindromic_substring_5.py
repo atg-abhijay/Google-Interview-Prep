41b7d501172537ec1fb4f5497e8eadc685692b0c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-27 00:03:21 -0500

    5 - Solve using approach used for 647. Palindromic Substrings
    - I am not considering this a normal solve since I had solved
    647 by using/learning the approach from the LeetCode discussion
    board.
    
    - Use the technique from 647 to find all the palindromes and then
    check for and track the longest palindrome.
    
    - The solution is a bit slow though: 2000 ms, 33%

--- String/longest_palindromic_substring_5.py
+++ String/longest_palindromic_substring_5.py
@@ -5,12 +5,41 @@ https://leetcode.com/problems/longest-palindromic-substring/
 
 
 class Solution:
+    def __init__(self):
+        self.longest_palind = None
+        self.longest_len = 0
+        self.str_len = 0
+        # self.palindromes = []
+
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        return ""
+        self.str_len = len(s)
+        for idx in range(self.str_len):
+            # 1. Will find odd length palindromes
+            # 2. Will find even length palindromes
+            self.find_palind(s, idx, idx)
+            self.find_palind(s, idx, idx+1)
+
+        # print(self.palindromes)
+        return self.longest_palind
+
+
+    def find_palind(self, string, left_idx, right_idx):
+        while left_idx >= 0 and right_idx < self.str_len:
+            if string[left_idx] == string[right_idx]:
+                # self.num_palinds += 1
+                if right_idx - left_idx + 1 > self.longest_len:
+                    self.longest_len = right_idx - left_idx + 1
+                    self.longest_palind = string[left_idx:right_idx+1]
+                # self.palindromes.append(string[left_idx:right_idx+1])
+            else:
+                break
+
+            left_idx -= 1
+            right_idx += 1
 
 
 def main():
