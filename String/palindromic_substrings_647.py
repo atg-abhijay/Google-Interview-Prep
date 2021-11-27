352fcfdb9ab62eabbfaadf3f6e700da212181128
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 22:49:09 -0500

    647 - Refactor code to use a single method for finding palindromes
    - The only difference between the functions for the even and odd
    length palindromes was the starting values for the left and right
    indices. Refactor the code to use a single function and then pass
    the relevant left and right indices as function arguments.

--- String/palindromic_substrings_647.py
+++ String/palindromic_substrings_647.py
@@ -7,39 +7,26 @@ https://leetcode.com/problems/palindromic-substrings/
 class Solution:
     def __init__(self):
         self.num_palinds = 0
-        self.palindromes = []
+        self.str_len = 0
+        # self.palindromes = []
 
     def countSubstrings(self, s):
-        str_len = len(s)
-        for odd_len_idx in range(str_len):
-            self.find_odd_palinds(s, odd_len_idx, str_len)
+        self.str_len = len(s)
+        for odd_len_idx in range(self.str_len):
+            self.find_palinds(s, odd_len_idx, odd_len_idx)
 
-        for even_len_idx in range(str_len):
-            self.find_even_palinds(s, even_len_idx, str_len)
+        for even_len_idx in range(self.str_len - 1):
+            self.find_palinds(s, even_len_idx, even_len_idx+1)
 
-        print(self.palindromes)
+        # print(self.palindromes)
         return self.num_palinds
 
 
-    def find_odd_palinds(self, string, odd_len_idx, str_len):
-        left_idx = right_idx = odd_len_idx
-        while left_idx >= 0 and right_idx < str_len:
+    def find_palinds(self, string, left_idx, right_idx):
+        while left_idx >= 0 and right_idx < self.str_len:
             if string[left_idx] == string[right_idx]:
                 self.num_palinds += 1
-                self.palindromes.append(string[left_idx:right_idx+1])
-            else:
-                break
-
-            left_idx -= 1
-            right_idx += 1
-
-
-    def find_even_palinds(self, string, even_len_idx, str_len):
-        left_idx, right_idx = even_len_idx, even_len_idx + 1
-        while left_idx >= 0 and right_idx < str_len:
-            if string[left_idx] == string[right_idx]:
-                self.num_palinds += 1
-                self.palindromes.append(string[left_idx:right_idx+1])
+                # self.palindromes.append(string[left_idx:right_idx+1])
             else:
                 break
 
