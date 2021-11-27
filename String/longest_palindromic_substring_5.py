685dcccafb61ad45e8859366581d90caaca764fc
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-11-26 23:59:18 -0500

    5. Longest Palindromic Substring: Initial commit

---
+++ String/longest_palindromic_substring_5.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/longest-palindromic-substring/
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ""
+
+
+def main():
+    print(Solution().longestPalindrome("cbbd"))
+
+
+if __name__ == "__main__":
+    main()
