bddd3ab83fc279439c5696ecd453f46bb1d0a0f7
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-02 01:36:12 -0500

    40 - Mostly solve the problem using approach from another problem
    - Solve the problem for the most part using the approach used for
    416. Partition Equal Subset Sum.
    
    - Do things in the same way as in starting from the beginning of
    the array and building up answers to whether it is possible to
    reach a target t using the array starting from idx - so, pairs of
    (idx, t) in the defaultdict.
    
    - Pass an additional 'path' variable that will be used to keep
    track of the current path along our DFS traversal. If the path
    ever reaches a point where the target is 0, append the path to
    the list of all paths.
    
    - Add some additional logic to the stuff from 416 since we want
    to know _all_ the paths, not just whether a path exists or not.
    For example, in the two branches where we're recursing into
    (idx + 1, t), once while including the current number and once
    while excluding it, add an additional condition to the if block.
    If the target is not in the defaultdict, then we need to recurse
    into idx + 1, same as before. But also now, if the sub-answer
    from idx + 1 is True, we want to recurse into it so that we can
    obtain the valid path.
    
    - Sort the candidates before starting because there was an
    observation that same paths were being added to the list (these
    same paths just had different orders for the same numbers). By
    sorting the numbers, I observed that the same paths now had the
    same orders as well (It isn't too bad to sort the candidates at
    all since #candidates is upper bounded by 100 in the problem).
    
    - In conjuction with the sorting, let the list of all paths be
    a set so that only unique paths are stored for the final result.
    
    - Also, swap the positions of the base cases. There might be cases
    where a target of 0 is achieved when we reach the end of candidates.
    In that case, we would like to add the path obtained in this
    situation, but 'candidates[idx:]' would cause the if-check to fail.
    Therefore, check for target == 0 first.
    
    172 / 175 test cases passed.
    
    - Getting Time Limit Exceeded. I suspect this is happening in the
    recursion step wherein we are redeveloping paths for
    (idx + 1, target) when we already know it to be True from a previous
    recursive call into it.

--- Dynamic-Programming/combination_sum_II_40.py
+++ Dynamic-Programming/combination_sum_II_40.py
@@ -4,18 +4,59 @@ https://leetcode.com/problems/combination-sum-ii/
 """
 
 
+from collections import defaultdict
+
+
 class Solution:
+    def __init__(self):
+        self.possible = defaultdict(lambda: defaultdict(bool))
+        self.paths = set()
+
     def combinationSum2(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        return [[]]
+        candidates.sort()
+        self.partition(0, target, candidates, [])
+        return self.paths
+
+    def partition(self, idx, target, candidates, path):
+        # Base cases
+        if target == 0:
+            self.possible[idx][target] = True
+            self.paths.add(tuple(path.copy()))
+            return True
+
+        if target < 0 or not candidates[idx:]:
+            self.possible[idx][target] = False
+            return False
+
+        # Exclude the number at idx
+        if target not in self.possible[idx + 1] or self.possible[idx + 1][target]:
+            exclude_result = self.partition(idx + 1, target, candidates, path)
+
+        exclude_result = self.possible[idx + 1][target]
+
+        # Include the number at idx
+        sub_target = target - candidates[idx]
+        if sub_target not in self.possible[idx + 1] or self.possible[idx + 1][sub_target]:
+            path.append(candidates[idx])
+            include_result = self.partition(idx + 1, sub_target, candidates, path)
+            path.pop()
+
+        include_result = self.possible[idx + 1][sub_target]
+        # for tgt in [target, target - candidates[idx]]:
+        #     if tgt not in self.possible[idx + 1]:
+        #         self.partition(idx + 1, tgt, candidates, path)
+                    # return True
+
+        self.possible[idx][target] = exclude_result or include_result
 
 
 def main():
-    print(Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
+    print(Solution().combinationSum2([1, 5, 11, 5], 11))
 
 
 if __name__ == "__main__":
