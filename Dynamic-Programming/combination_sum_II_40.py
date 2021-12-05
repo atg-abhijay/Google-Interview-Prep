45a5a079ec2a647f309bc9b2752067705b08bf60
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-05 15:17:51 -0500

    40 - Remove the boolean tracking of results
    - The boolean result tracking is not needed. Simply check if an
    (idx, target) combination has been calculated previously or not.
    Then, use that previous/fresh result to update the paths for the
    current (idx, target) combination.

--- Dynamic-Programming/combination_sum_II_40.py
+++ Dynamic-Programming/combination_sum_II_40.py
@@ -26,30 +26,26 @@ class Solution:
         # Base cases
         if target == 0:
             self.possible[idx][target].add(tuple())
-            return True
+            return
 
         if target < 0 or not candidates[idx:]:
-            return False
+            return
 
-        exclude_result = True
         # Exclude the number at idx
         if target not in self.possible[idx + 1]:
-            exclude_result = self.partition(idx + 1, target, candidates)
+            self.partition(idx + 1, target, candidates)
 
-        if exclude_result:
-            self.possible[idx][target].update(self.possible[idx+1][target])
+        self.possible[idx][target].update(self.possible[idx+1][target])
 
         # Include the number at idx
-        include_result = True
         sub_target = target - candidates[idx]
         if sub_target not in self.possible[idx + 1]:
-            include_result = self.partition(idx + 1, sub_target, candidates)
+            self.partition(idx + 1, sub_target, candidates)
 
-        if include_result:
-            for sub_path in self.possible[idx+1][sub_target]:
-                self.possible[idx][target].add(tuple([candidates[idx]]) + sub_path)
+        for sub_path in self.possible[idx+1][sub_target]:
+            self.possible[idx][target].add(tuple([candidates[idx]]) + sub_path)
 
-        return exclude_result or include_result
+        return
 
 
 def main():
