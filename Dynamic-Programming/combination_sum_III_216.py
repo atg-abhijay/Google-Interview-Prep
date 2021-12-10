a2ca1023590158124afb0e51ef1c74cb390b7f55
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-10 18:52:38 -0500

    216 - Simplify structure of defaultdict
    - Instead of having a 3-layered defaultdict, store the idx, target
    and path_size as a tuple. A tuple works as a key in a dictionary
    and since we're not changing these 3 parameters themselves, we're
    good to go once they're inserted into the dictionary.
    
    - This simplifies the if-block checks while partitioning as well.
    Now, we can simply check for the existence of the tuple in the
    dictionary.

--- Dynamic-Programming/combination_sum_III_216.py
+++ Dynamic-Programming/combination_sum_III_216.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 class Solution(object):
     def __init__(self):
-        self.paths = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        self.paths = defaultdict(list)
         self.size_limit = 0
 
     def combinationSum3(self, k, n):
@@ -18,34 +18,33 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-        candidates = [x for x in range(1, 10)]
         self.size_limit = k
-        self.partition(0, n, candidates, k)
-        return self.paths[0][n][k]
+        self.partition(0, n, list(range(1, 10)), k)
+        return self.paths[(0, n, k)]
 
     def partition(self, idx, target, candidates, path_size):
-        self.paths[idx][target][path_size] = []
+        self.paths[(idx, target, path_size)] = []
         # Base cases
         if target == 0 and path_size == 0:
-            self.paths[idx][target][path_size].append([])
+            self.paths[(idx, target, path_size)].append([])
             return
 
         if not candidates[idx:] or target < candidates[idx] or path_size == 0:
             return
 
         # Exclude the number at idx
-        if target not in self.paths[idx + 1] or path_size not in self.paths[idx + 1][target]:
+        if (idx + 1, target, path_size) not in self.paths:
             self.partition(idx + 1, target, candidates, path_size)
 
-        self.paths[idx][target][path_size].extend(self.paths[idx + 1][target][path_size])
+        self.paths[(idx, target, path_size)].extend(self.paths[(idx + 1, target, path_size)])
 
         # Include the number at idx
         sub_target = target - candidates[idx]
-        if sub_target not in self.paths[idx + 1] or path_size - 1 not in self.paths[idx + 1][sub_target]:
+        if (idx + 1, sub_target, path_size - 1) not in self.paths:
             self.partition(idx + 1, sub_target, candidates, path_size - 1)
 
-        for sub_path in self.paths[idx + 1][sub_target][path_size - 1]:
-            self.paths[idx][target][path_size].append([candidates[idx]] + sub_path)
+        for sub_path in self.paths[(idx + 1, sub_target, path_size - 1)]:
+            self.paths[(idx, target, path_size)].append([candidates[idx]] + sub_path)
 
         return
 
