9d765f655f526a869874dc42b709a099db3dc73c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-10 13:23:47 -0500

    216. Combination Sum III: Initial commit

---
+++ Dynamic-Programming/combination_sum_III_216.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/combination-sum-iii/
+"""
+
+
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().combinationSum3(3, 9))
+
+
+if __name__ == "__main__":
+    main()
