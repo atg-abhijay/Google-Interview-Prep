938d17c6e87f3b0e262086e77ad665eb576f23ed
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-10 13:25:56 -0500

    216 - Bring over work from 40. Combination Sum II as a base, starting point

--- Dynamic-Programming/combination_sum_III_216.py
+++ Dynamic-Programming/combination_sum_III_216.py
@@ -4,14 +4,48 @@ https://leetcode.com/problems/combination-sum-iii/
 """
 
 
+from collections import defaultdict
+
+
 class Solution(object):
+    def __init__(self):
+        self.paths = defaultdict(lambda: defaultdict(set))
+
     def combinationSum3(self, k, n):
         """
         :type k: int
         :type n: int
         :rtype: List[List[int]]
         """
-        return [[]]
+        candidates.sort()
+        self.partition(0, target, candidates)
+        return self.paths[0][target]
+
+    def partition(self, idx, target, candidates):
+        self.paths[idx][target] = set()
+        # Base cases
+        if target == 0:
+            self.paths[idx][target].add(tuple())
+            return
+
+        if not candidates[idx:] or target < candidates[idx]:
+            return
+
+        # Exclude the number at idx
+        if target not in self.paths[idx + 1]:
+            self.partition(idx + 1, target, candidates)
+
+        self.paths[idx][target].update(self.paths[idx + 1][target])
+
+        # Include the number at idx
+        sub_target = target - candidates[idx]
+        if sub_target not in self.paths[idx + 1]:
+            self.partition(idx + 1, sub_target, candidates)
+
+        for sub_path in self.paths[idx + 1][sub_target]:
+            self.paths[idx][target].add(tuple([candidates[idx]]) + sub_path)
+
+        return
 
 
 def main():
