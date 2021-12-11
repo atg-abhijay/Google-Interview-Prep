b63bfa003bbc5e4dd59827e7cb493504bea731f1
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-11 17:33:23 -0500

    377 - Create a base off of the initial work for 39. Combination Sum
    - The work is based on the commit 20f63ff. In this commit, I had
    come up with a way to generate all the combinations but was getting
    stuck on how to deal with non-unique combinations.
    
    - _But_, in this problem, we want the permutations of the different
    paths, i.e. we want the non-unique combinations as well.
    
    8 / 15 test cases passed.
    
    - Getting Time Limit Exceeded. Test case: [4, 2, 1], 32

--- Dynamic-Programming/combination_sum_IV_377.py
+++ Dynamic-Programming/combination_sum_IV_377.py
@@ -4,60 +4,22 @@ https://leetcode.com/problems/combination-sum-iv/
 """
 
 
-from collections import defaultdict, Counter
-from functools import reduce
-from math import factorial
-from operator import mul
-
-
 class Solution:
-    def __init__(self):
-        self.paths = defaultdict(set)
-
     def combinationSum4(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        nums.sort()
-        self.partition(0, target, nums)
-        num_ways = 0
-        for path in self.paths[(0, target)]:
-            numtr = factorial(len(path))
-            counter = Counter(path)
-            demtr = reduce(mul, map(factorial, counter.values()))
-            num_ways += numtr/demtr
+        combinations = [[] for _ in range(target + 1)]
+        for tgt in range(1, target + 1):
+            for candt in nums:
+                if tgt - candt > 0 and combinations[tgt - candt]:
+                    combinations[tgt] += [combn + [candt] for combn in combinations[tgt - candt]]
+                if tgt - candt == 0:
+                    combinations[tgt] += [[candt]]
 
-        return int(num_ways)
-
-    def partition(self, idx, target, nums):
-        current_params = (idx, target)
-        self.paths[current_params] = set()
-        # Base cases
-        if target == 0:
-            self.paths[current_params].add(tuple())
-            return
-
-        if not nums[idx:] or target < nums[idx]:
-            return
-
-        # Exclude the number at idx
-        exclusion_params = (idx + 1, target)
-        if exclusion_params not in self.paths:
-            self.partition(*exclusion_params, nums)
-
-        self.paths[current_params].update(self.paths[exclusion_params])
-
-        # Include the number at idx
-        inclusion_params = (idx + 1, target - nums[idx])
-        if inclusion_params not in self.paths:
-            self.partition(*inclusion_params, nums)
-
-        for sub_path in self.paths[inclusion_params]:
-            self.paths[current_params].add(tuple([nums[idx]]) + sub_path)
-
-        return
+        return combinations[target]
 
 
 def main():
