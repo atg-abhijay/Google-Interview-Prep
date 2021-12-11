d6b6272eb50bee3ab8d80508c705bd889d8d9ced
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-11 17:11:52 -0500

    377 - Bring over work from 40. Combination Sum II as a base, starting point

--- Dynamic-Programming/combination_sum_IV_377.py
+++ Dynamic-Programming/combination_sum_IV_377.py
@@ -4,14 +4,50 @@ https://leetcode.com/problems/combination-sum-iv/
 """
 
 
+from collections import defaultdict
+
+
 class Solution:
+    def __init__(self):
+        self.paths = defaultdict(set)
+
     def combinationSum4(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        return -1
+        nums.sort()
+        self.partition(0, target, nums)
+        return self.paths[(0, target)]
+
+    def partition(self, idx, target, nums):
+        current_params = (idx, target)
+        self.paths[current_params] = set()
+        # Base cases
+        if target == 0:
+            self.paths[current_params].add(tuple())
+            return
+
+        if not nums[idx:] or target < nums[idx]:
+            return
+
+        # Exclude the number at idx
+        exclusion_params = (idx + 1, target)
+        if exclusion_params not in self.paths:
+            self.partition(*exclusion_params, nums)
+
+        self.paths[current_params].update(self.paths[exclusion_params])
+
+        # Include the number at idx
+        inclusion_params = (idx + 1, target - nums[idx])
+        if inclusion_params not in self.paths:
+            self.partition(*inclusion_params, nums)
+
+        for sub_path in self.paths[inclusion_params]:
+            self.paths[current_params].add(tuple([nums[idx]]) + sub_path)
+
+        return
 
 
 def main():
