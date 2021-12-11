c3caba357559f2d5ad0028d1397c417671f03300
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-11 16:59:51 -0500

    377. Combination Sum IV: Initial commit

---
+++ Dynamic-Programming/combination_sum_IV_377.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/combination-sum-iv/
+"""
+
+
+class Solution:
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().combinationSum4([1, 2, 3], 4))
+
+
+if __name__ == "__main__":
+    main()
