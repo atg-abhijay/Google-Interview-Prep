395a9930ca2965c392e1347e8741c53af5dd062c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-14 19:04:38 -0500

    1791. Find Center of Star Graph: Initial commit

---
+++ Graph/find_center_star_graph_1791.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/find-center-of-star-graph/
+"""
+
+
+class Solution:
+    def findCenter(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().findCenter([[1, 2], [5, 1], [1, 3], [1, 4]]))
+
+
+if __name__ == "__main__":
+    main()
