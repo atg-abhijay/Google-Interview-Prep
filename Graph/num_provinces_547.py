3347a70494de0b672be8f5291130df5ff35a8b4b
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-15 18:36:25 -0500

    547. Number of Provinces: Initial commit

---
+++ Graph/num_provinces_547.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/number-of-provinces/
+"""
+
+
+class Solution:
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().findCircleNum([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+
+
+if __name__ == "__main__":
+    main()
