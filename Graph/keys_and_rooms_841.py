9e9278437e4423d0b83f95d26989837a6d436ae4
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-14 22:46:20 -0500

    841. Keys and Rooms: Initial commit

---
+++ Graph/keys_and_rooms_841.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/keys-and-rooms/
+"""
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        return False
+
+
+def main():
+    print(Solution().canVisitAllRooms([[1, 3], [3, 0, 1], [2], [0]]))
+
+
+if __name__ == "__main__":
+    main()
