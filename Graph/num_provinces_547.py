75ba0e60689714bc2f0ee03d8a5144f5c000016b
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-15 18:44:35 -0500

    Solve 547. Number of Provinces
    - Run multiple instances of BFS on a possibly disconnected graph.
    Each run of the BFS gives us the connections of the cities to one
    another, i.e. we get a province. The number of times we can
    execute a BFS on the input graph gives us the number of provinces.
    
    - Runtime: 188 ms, 75%

--- Graph/num_provinces_547.py
+++ Graph/num_provinces_547.py
@@ -4,13 +4,28 @@ https://leetcode.com/problems/number-of-provinces/
 """
 
 
+from collections import deque
+
+
 class Solution:
     def findCircleNum(self, isConnected):
         """
         :type isConnected: List[List[int]]
         :rtype: int
         """
-        return -1
+        num_provinces = 0
+        unvisited_cities = set(range(len(isConnected)))
+        while unvisited_cities:
+            queue = deque([unvisited_cities.pop()])
+            num_provinces += 1
+            while queue:
+                city = queue.popleft()
+                for idx, is_nbr in enumerate(isConnected[city]):
+                    if idx in unvisited_cities and is_nbr:
+                        unvisited_cities.remove(idx)
+                        queue.append(idx)
+
+        return num_provinces
 
 
 def main():
