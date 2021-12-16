32b7f1f0ebc4937cfa31ccb961a8b1abdf85f875
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-15 21:40:15 -0500

    684. Redundant Connection: Initial commit

---
+++ Graph/redundant_connection_684.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/redundant-connection/
+"""
+
+
+class Solution:
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().findRedundantConnection([[1, 2], [1, 3], [2, 3]]))
+
+
+if __name__ == "__main__":
+    main()
