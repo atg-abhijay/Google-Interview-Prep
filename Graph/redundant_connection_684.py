540532bd5f7cb00bcc0b561739ace4ed98bb9491
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-17 17:06:05 -0500

    684 - Extract out some variables and return extra edge earlier
    - With the way the solution functions and given that there's only
    one extra edge, as soon as we find the extra edge, we can return
    it. We don't have to wait for the whole for-loop to finish before
    returning the extra edge.
    
    - The runtime is improved from 60 ms, 52% to 52 ms, 87%!

--- Graph/redundant_connection_684.py
+++ Graph/redundant_connection_684.py
@@ -16,35 +16,35 @@ class Solution:
         """
         components = defaultdict(set)
         node_cmpts = defaultdict(lambda: float('inf'))
-        extra_edge, curr_val = [], 1
+        curr_val = 1
         for node_u, node_v in edges:
             u_cmpt, v_cmpt = node_cmpts[node_u], node_cmpts[node_v]
+            # Nodes that belong to the same
+            # components as nodes u and v respectively
+            u_nodes, v_nodes = components[u_cmpt], components[v_cmpt]
             if u_cmpt == v_cmpt != float('inf'):
-                extra_edge = [node_u, node_v]
-                continue
+                return [node_u, node_v]
 
             cmpt_val = min(curr_val, u_cmpt, v_cmpt)
             components[cmpt_val].update([node_u, node_v])
             node_cmpts[node_u] = node_cmpts[node_v] = cmpt_val
-            components[cmpt_val].update(*[components[u_cmpt], components[v_cmpt]])
+            components[cmpt_val].update(*[u_nodes, v_nodes])
 
             cmpt_to_clear = float('inf')
             if cmpt_val == curr_val:
                 curr_val += 1
-                nodes_to_update = [components[u_cmpt], components[v_cmpt]]
+                nodes_to_update = [u_nodes, v_nodes]
             elif cmpt_val == u_cmpt:
-                nodes_to_update = [components[v_cmpt]]
+                nodes_to_update = [v_nodes]
                 cmpt_to_clear = v_cmpt
             else:
-                nodes_to_update = [components[u_cmpt]]
+                nodes_to_update = [u_nodes]
                 cmpt_to_clear = u_cmpt
 
             for node in chain(*nodes_to_update):
                 node_cmpts[node] = cmpt_val
 
             components[cmpt_to_clear].clear()
-
-        return extra_edge
 
 
 def main():
