0058d6588fd335f29d753ee29165feadf68caa5d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-18 14:17:39 -0500

    765. Couples Holding Hands: Initial commit

---
+++ Graph/couples_holding_hands_765.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/couples-holding-hands/
+"""
+
+
+class Solution:
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().minSwapsCouples([3, 2, 0, 1]))
+
+
+if __name__ == "__main__":
+    main()
