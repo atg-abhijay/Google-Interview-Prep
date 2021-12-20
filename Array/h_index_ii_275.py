63b375402ec814f91ad8f1d23146166d175d57cd
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-20 18:20:20 -0500

    275. H-Index II: Initial commit

---
+++ Array/h_index_ii_275.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/h-index-ii/
+"""
+
+
+class Solution:
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().hIndex([0, 1, 3, 5, 6]))
+
+
+if __name__ == "__main__":
+    main()
