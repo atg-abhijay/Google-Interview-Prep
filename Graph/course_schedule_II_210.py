4de6367aaab240e103b3f14a3d9a5cadace13b6d
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-24 16:41:14 -0500

    210. Course Schedule II: Initial commit

---
+++ Graph/course_schedule_II_210.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/course-schedule-ii/
+"""
+
+
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        return []
+
+
+def main():
+    print(Solution().findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
+
+
+if __name__ == "__main__":
+    main()
