ee530416851a8059a252fc3ae2e7dd8b50792b0c
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-24 16:24:05 -0500

    Solve 1615. Maximal Network Rank
    - Build an undirected graph out of the input list roads. Generate
    all the pairs of cities possible out of n cities. Determine the
    rank for each pair. If the cities are connected to each other,
    the rank will include that edge twice, so subtract 1 from the
    rank. Keep track of the maximum rank amongst the ranks of all
    the pairs.
    
    - Runtime: 336 ms, 84%

--- Graph/maximal_network_rank_1615.py
+++ Graph/maximal_network_rank_1615.py
@@ -4,6 +4,10 @@ https://leetcode.com/problems/maximal-network-rank/
 """
 
 
+from collections import defaultdict
+from itertools import combinations
+
+
 class Solution:
     def maximalNetworkRank(self, n, roads):
         """
@@ -11,7 +15,20 @@ class Solution:
         :type roads: List[List[int]]
         :rtype: int
         """
-        return 0
+        graph = defaultdict(set)
+        for city_a, city_b in roads:
+            graph[city_a].add(city_b)
+            graph[city_b].add(city_a)
+
+        max_network_rank = 0
+        for city_a, city_b in combinations(range(n), 2):
+            pair_rank = len(graph[city_a]) + len(graph[city_b])
+            if city_a in graph[city_b]:
+                pair_rank -= 1
+
+            max_network_rank = max(max_network_rank, pair_rank)
+
+        return max_network_rank
 
 
 def main():
