d18f4a4dbad22e158641e4ad6e5a9506c8194520
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2021-12-26 23:38:52 -0500

    2101 - Refactor the code to not use the self.* variables
    - Runtime changed from 1028 ms, 45% to 824 ms, 78%

--- Graph/detonate_maximum_bombs_2101.py
+++ Graph/detonate_maximum_bombs_2101.py
@@ -9,10 +9,6 @@ from itertools import combinations
 
 
 class Solution:
-    def __init__(self):
-        self.visited = set()
-        self.max_detonations = []
-
     def maximumDetonation(self, bombs):
         """
         :type bombs: List[List[int]]
@@ -31,21 +27,22 @@ class Solution:
             if distance <= b_radius ** 2:
                 graph[b_idx].add(a_idx)
 
-        num_bombs = len(bombs)
-        self.max_detonations = [1] * num_bombs
-        for bomb in range(num_bombs):
-            self.visited.add(bomb)
-            self.max_detonations[bomb] = self.perform_dfs(graph, bomb)
-            self.visited.clear()
+        max_detonations, visited = 0, set()
+        for bomb in range(len(bombs)):
+            visited.add(bomb)
+            max_detonations = max(
+                max_detonations, self.perform_dfs(graph, bomb, visited)
+            )
+            visited.clear()
 
-        return max(self.max_detonations)
+        return max_detonations
 
-    def perform_dfs(self, graph, bomb):
+    def perform_dfs(self, graph, bomb, visited):
         num_detonations = 1
         for nbr_bomb in graph[bomb]:
-            if nbr_bomb not in self.visited:
-                self.visited.add(nbr_bomb)
-                num_detonations += self.perform_dfs(graph, nbr_bomb)
+            if nbr_bomb not in visited:
+                visited.add(nbr_bomb)
+                num_detonations += self.perform_dfs(graph, nbr_bomb, visited)
 
         return num_detonations
 
