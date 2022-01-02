378fc5ff991939e002682945fad97322383a6bb2
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2022-01-02 14:06:28 -0500

    Solve 997. Find the Town Judge
    - Revert back to using the code from 5693adece5c9b3aa41a90ca012f4e6943f0e6bde
    and add an if-condition to handle the edge case.
    
    - The defaultdict works better for the graph I think because we
    only put data in it as and when we need it as opposed to
    instantiating everything at the beginning.
    
    - The runtime is now: 712 ms, 91% (don't know if this reliable
    or not though, in terms of the fluctuations in running times given
    by LeetCode).

--- Graph/find_town_judge_997.py
+++ Graph/find_town_judge_997.py
@@ -14,14 +14,17 @@ class Solution:
         :type trust: List[List[int]]
         :rtype: int
         """
-        graph = [[0, 0] for _ in range(n)]
-        for person_a, person_b in trust:
-            graph[person_a-1][0] += 1
-            graph[person_b-1][1] += 1
+        if n == 1 and not trust:
+            return 1
 
-        for person, trust_values in enumerate(graph):
+        graph = defaultdict(lambda: [0, 0])
+        for person_a, person_b in trust:
+            graph[person_a][0] += 1
+            graph[person_b][1] += 1
+
+        for person, trust_values in graph.items():
             if trust_values == [0, n-1]:
-                return person + 1
+                return person
 
         return -1
 
