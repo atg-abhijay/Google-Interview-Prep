ef5f957b7075fbf01ace47aee5b8bede63e7822e
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2022-01-02 11:58:54 -0500

    997. Find the Town Judge: Initial commit

---
+++ Graph/find_town_judge_997.py
@@ -0,0 +1,22 @@
+"""
+URL of problem:
+https://leetcode.com/problems/find-the-town-judge/
+"""
+
+
+class Solution:
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        return -1
+
+
+def main():
+    print(Solution().findJudge(3, [[1, 3], [2, 3], [3, 1]]))
+
+
+if __name__ == "__main__":
+    main()
