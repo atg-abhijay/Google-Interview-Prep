039c82815dc9c0ba928a59a536e16d73631f47b6
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2022-01-03 23:29:23 -0500

    797. All Paths From Source to Target: Initial commit

---
+++ Graph/all_paths_source_target_797.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/all-paths-from-source-to-target/
+"""
+
+
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[]]
+
+
+def main():
+    print(Solution().allPathsSourceTarget([[4, 3, 1], [3, 2, 4], [3], [4], []]))
+
+
+if __name__ == "__main__":
+    main()
