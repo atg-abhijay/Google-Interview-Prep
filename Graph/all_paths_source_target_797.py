67cddefc13b6f48dce4ba7baa8dd68a697ef868b
Abhijay Gupta <23014438+atg-abhijay@users.noreply.github.com>
2022-01-03 23:49:49 -0500

    Solve 797. All Paths From Source to Target
    - Solve the problem by caching sub-paths. Start from node 0 and
    find paths leading to the target. Once the paths are determined
    from a sub-source, store those paths so that they can be reused
    by any other node that arrives at this sub-source.
    
    - The idea is similar to the technique used for 40. Combination
    Sum II (latest commit 2b2ed71) and 216. Combination Sum III
    (latest commit 2dc73cb) wherein paths were cached.
    
    - Successive submissions of the solution gave runtimes of:
    133 ms, 13%; 163 ms, 6% and 96 ms, 88%.

--- Graph/all_paths_source_target_797.py
+++ Graph/all_paths_source_target_797.py
@@ -4,13 +4,36 @@ https://leetcode.com/problems/all-paths-from-source-to-target/
 """
 
 
+from collections import defaultdict
+
+
 class Solution:
+    def __init__(self):
+        self.paths_from_node = defaultdict(list)
+        self.target = -1
+
     def allPathsSourceTarget(self, graph):
         """
         :type graph: List[List[int]]
         :rtype: List[List[int]]
         """
-        return [[]]
+        self.target = len(graph) - 1
+        return self.find_paths_from_source(graph, 0)
+
+    def find_paths_from_source(self, graph, source):
+        if source in self.paths_from_node:
+            return self.paths_from_node[source]
+
+        if source == self.target:
+            self.paths_from_node[source] = [[source]]
+            return self.paths_from_node[source]
+
+        for nbr in graph[source]:
+            sub_paths = self.find_paths_from_source(graph, nbr)
+            for sub_path in sub_paths:
+                self.paths_from_node[source].append([source] + sub_path)
+
+        return self.paths_from_node[source]
 
 
 def main():
